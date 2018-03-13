95ec9c9c61211719a95222514f3a6569b7283afb
Matt Painter <mp2u16@ecs.soton.ac.uk>
2018-03-13 17:46:46 +0000

    Feature/trainer (#11)
    * Add basic bink main trainer
    
    * Add util file
    
    * Add simple example
    
    * Fix bugs in bink.py
    
    * Add support for multiple callbacks
    
    * Set eval mode for model validation

--- framework/callbacks/callbacks.py
+++ framework/callbacks/callbacks.py
@@ -31,6 +31,49 @@ class Callback(object):
         pass
 
 
+class CallbackList(Callback):
+    def __init__(self, callback_list):
+        super().__init__()
+        if callback_list is None:
+            self._list = []
+        else:
+            self._list = callback_list
+        
+    def _for_list(self, function):
+        for callback in self._list:
+            function(callback)
+        
+    def on_start(self, state):
+        self._for_list(lambda callback: callback.on_start(state))
+
+    def on_start_epoch(self, state):
+        self._for_list(lambda callback: callback.on_start_epoch(state))
+
+    def on_sample(self, state):
+        self._for_list(lambda callback: callback.on_sample(state))
+
+    def on_forward(self, state):
+        self._for_list(lambda callback: callback.on_forward(state))
+
+    def on_forward_criterion(self, state):
+        self._for_list(lambda callback: callback.on_forward_criterion(state))
+
+    def on_backward_criterion(self, state):
+        self._for_list(lambda callback: callback.on_backward_criterion(state))
+
+    def on_backward(self, state):
+        self._for_list(lambda callback: callback.on_backward(state))
+
+    def on_update_parameters(self, state):
+        self._for_list(lambda callback: callback.on_update_parameters(state))
+
+    def on_end_epoch(self, state):
+        self._for_list(lambda callback: callback.on_end_epoch(state))
+
+    def on_end(self, state):
+        self._for_list(lambda callback: callback.on_end(state))
+
+
 class KerasCallbackWrapper(Callback):
     def __init__(self, keras_callback):
         super().__init__()
---
+++ utils.py
@@ -0,0 +1,33 @@
+from torch.utils.data import DataLoader, TensorDataset
+import torch
+
+def train_valid_splitter(x, y, split, shuffle=True):
+    num_samples_x = x.shape[0]
+    num_valid_samples = torch.floor(num_samples_x * split)
+
+    if shuffle:
+        indicies = torch.randperm(num_samples_x)
+        x, y = x[indicies], y[indicies]
+
+    x_val, y_val = x[:num_valid_samples], y[:num_valid_samples]
+    x, y = x[num_valid_samples:], y[num_valid_samples:]
+
+    return x, y, x_val, y_val
+
+
+def get_train_valid_sets(x, y, validation_data, validation_split, shuffle=True):
+
+    valset = None
+
+    if validation_data is not None:
+        x_val, y_val = validation_data
+    elif validation_split > 0.0:
+        x, y, x_val, y_val = train_valid_splitter(x, y, validation_split, shuffle=shuffle)
+    else:
+        x_val, y_val = None, None
+
+    trainset = TensorDataset(x, y)
+    if x_val is not None and y_val is not None:
+        valset = TensorDataset(x_val, y_val)
+
+    return trainset, valset
