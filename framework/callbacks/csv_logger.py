045cf73892a53ede04b22cc5e55afcc59969b5ed
Matt Painter <mp2u16@ecs.soton.ac.uk>
2018-03-14 15:56:15 +0000

    Add csv logger callback (#22)

---
+++ framework/callbacks/csv_logger.py
@@ -0,0 +1,50 @@
+from framework.callbacks.callbacks import Callback
+import csv
+
+
+class CSVLogger(Callback):
+    def __init__(self, filename, separator=',', batch_granularity=False, write_header=True, append=False):
+        super().__init__()
+        self.batch_granularity = batch_granularity
+        self.filename = filename
+        self.separator = separator
+        if append:
+            filemode = 'a+'
+        else:
+            filemode = 'w+'
+        self.csvfile = open(self.filename, filemode, newline='')
+        self.write_header = write_header
+
+    def on_update_parameters(self, state):
+        super().on_update_parameters(state)
+        if self.batch_granularity:
+            self._write_to_dict(state)
+
+    def on_end_epoch(self, state):
+        super().on_end_epoch(state)
+        self._write_to_dict(state)
+
+    def on_end(self, state):
+        super().on_end(state)
+        self.csvfile.close()
+
+    def _write_to_dict(self, state):
+        fields = self._get_field_dict(state)
+
+        if self.write_header:
+            self.writer = csv.DictWriter(self.csvfile, fieldnames=fields.keys(), delimiter=self.separator)
+            self.writer.writeheader()
+            self.write_header = False
+
+        self.writer.writerow(fields)
+        self.csvfile.flush()
+
+    def _get_field_dict(self, state):
+        fields = {'epoch': state['epoch']}
+
+        if self.batch_granularity:
+            fields.update({'batch': state['t']})
+
+        fields.update(state['metrics'])
+
+        return fields
