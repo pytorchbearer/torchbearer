ba70ec760250ed86f1117182862b9a652118f748
Matt <mp2u16@soton.ac.uk>
2018-03-25 17:33:07 +0100

    Add cross validation dataset

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
-from torch.utils.data import DataLoader, TensorDataset
+from torch.utils.data import DataLoader, TensorDataset,  Dataset
 import torch
+from random import shuffle as shuffle_list
 
 def train_valid_splitter(x, y, split, shuffle=True):
     num_samples_x = x.shape[0]
@@ -31,3 +32,61 @@ def get_train_valid_sets(x, y, validation_data, validation_split, shuffle=True):
         valset = TensorDataset(x_val, y_val)
 
     return trainset, valset
+
+
+class DatasetCrossValidation:
+    def __init__(self, dataset, num_folds=1, valid_split=0.1, shuffle=False):
+        super().__init__()
+        self.dataset = dataset
+        self.num_folds = num_folds
+        self.valid_len = int(len(dataset)*valid_split)
+        self.ids = range(len(dataset))
+        self.current_fold = 0
+
+        if shuffle:
+            shuffle_list(self.ids)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current_fold > self.num_folds:
+            raise StopIteration
+
+        val_start = self.current_fold*self.valid_len
+        valid_ids = self.ids[val_start:val_start+self.valid_len]
+
+        sets = DatasetValidation(self.dataset, valid_ids)
+        return sets.get_train_dataset(), sets.get_valid_dataset()
+
+
+class DatasetValidation:
+    def __init__(self, dataset, valid_ids, shuffle=False):
+        super().__init__()
+        self.valid_ids = valid_ids
+        self.train_ids = [x for x in range(len(dataset)) if x not in valid_ids]
+        self.dataset = dataset
+        self.shuffle=shuffle
+
+    def get_train_dataset(self):
+        return SubsetDataset(self.dataset, self.train_ids, shuffle=self.shuffle)
+
+    def get_valid_dataset(self):
+        return SubsetDataset(self.dataset, self.valid_ids, shuffle=self.shuffle)
+
+
+class SubsetDataset(Dataset):
+    def __init__(self, dataset, ids, shuffle=False):
+        super().__init__()
+        self.dataset = dataset
+        self.ids = ids
+
+        if shuffle:
+            shuffle_list(self.ids)
+
+    def __getitem__(self, index):
+        return self.dataset.__getitem__(self.ids[index])
+
+    def __len__(self):
+        return len(self.ids)
+
