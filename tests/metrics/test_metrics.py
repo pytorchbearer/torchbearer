dde027a6d28e73c29959f2922f766098143e7f89
Ethan Harris <ewah1g13@soton.ac.uk>
2018-03-26 18:07:32 +0100

    Add metrics test (#62)

---
+++ tests/metrics/test_metrics.py
@@ -0,0 +1,51 @@
+import unittest
+from unittest.mock import Mock
+
+from bink.metrics import MetricList, Metric
+
+
+class TestMetricList(unittest.TestCase):
+    def test_default_acc(self):
+        metric = MetricList(['acc'])
+        self.assertTrue(metric.metric_list[0].name == 'running_acc', msg='running_acc not in: ' + str(metric.metric_list))
+
+    def test_default_loss(self):
+        metric = MetricList(['loss'])
+        self.assertTrue(metric.metric_list[0].name == 'running_loss', msg='running_loss not in: ' + str(metric.metric_list))
+
+    def test_evaluate_dict(self):
+        my_mock = Metric('test')
+        my_mock.evaluate = Mock(return_value=-1)
+        metric = MetricList([my_mock])
+        result = metric.evaluate_dict({'state': -1})
+        self.assertEqual({'train_test': -1}, result)
+        my_mock.evaluate.assert_called_once_with({'state': -1})
+
+    def test_evaluate_final_dict(self):
+        my_mock = Metric('test')
+        my_mock.evaluate_final = Mock(return_value=-1)
+        metric = MetricList([my_mock])
+        result = metric.evaluate_final_dict({'state': -1})
+        self.assertEqual({'train_test': -1}, result)
+        my_mock.evaluate_final.assert_called_once_with({'state': -1})
+
+    def test_train(self):
+        my_mock = Metric('test')
+        my_mock.train = Mock(return_value=None)
+        metric = MetricList([my_mock])
+        metric.train()
+        my_mock.train.assert_called_once()
+
+    def test_eval(self):
+        my_mock = Metric('test')
+        my_mock.eval = Mock(return_value=None)
+        metric = MetricList([my_mock])
+        metric.eval()
+        my_mock.eval.assert_called_once()
+
+    def test_reset(self):
+        my_mock = Metric('test')
+        my_mock.reset = Mock(return_value=None)
+        metric = MetricList([my_mock])
+        metric.reset({'state': -1})
+        my_mock.reset.assert_called_once_with({'state': -1})
