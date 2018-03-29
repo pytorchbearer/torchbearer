694b72ae656247ba2bfee7dbc1631c0a7568a5eb
Ethan Harris <ewah1g13@soton.ac.uk>
2018-03-29 19:51:32 +0100

    Add tensorboard feature plot (#86)

--- bink/callbacks/tensor_board.py
+++ bink/callbacks/tensor_board.py
@@ -71,12 +71,18 @@ class TensorBoardImageVis(Callback):
                  comment='bink',
                  num_images=100,
                  avg_pool_size=1,
-                 avg_feature_channels=True):
+                 avg_data_channels=True,
+                 write_data=True,
+                 write_features=True,
+                 features_key='y_pred'):
         self.log_dir = log_dir
         self.comment = comment
         self.num_images = num_images
         self.avg_pool_size = avg_pool_size
-        self.avg_feature_channels = avg_feature_channels
+        self.avg_data_channels = avg_data_channels
+        self.write_data = write_data
+        self.write_features = write_features
+        self.features_key = features_key
 
         self._writer = None
 
@@ -88,7 +94,6 @@ class TensorBoardImageVis(Callback):
 
     def on_step_validation(self, state):
         if not self.done:
-
             x = state['x'].data.clone()
 
             if len(x.size()) == 3:
@@ -96,35 +101,60 @@ class TensorBoardImageVis(Callback):
 
             x = F.avg_pool2d(Variable(x, volatile=True), self.avg_pool_size).data
 
-            if self.avg_feature_channels:
-                feature = torch.mean(x, 1)
-            else:
-                feature = x
+            data = None
 
-            feature = feature.view(feature.size(0), -1)
+            if state['epoch'] == 0 and self.write_data:
+                if self.avg_data_channels:
+                    data = torch.mean(x, 1)
+                else:
+                    data = x
+
+                data = data.view(data.size(0), -1)
+
+            feature = None
+
+            if self.write_features:
+                feature = state[self.features_key].data.clone()
+                feature = feature.view(feature.size(0), -1)
+
             label = state['y_true'].data.clone()
 
             if state['t'] == 0:
+                remaining = self.num_images if self.num_images < label.size(0) else label.size(0)
 
-                remaining = self.num_images if self.num_images < feature.size(0) else feature.size(0)
-
-                self._features = feature[:remaining].cpu()
                 self._images = x[:remaining].cpu()
                 self._labels = label[:remaining].cpu()
+
+                if data is not None:
+                    self._data = data[:remaining].cpu()
+
+                if feature is not None:
+                    self._features = feature[:remaining].cpu()
             else:
+                remaining = self.num_images - self._labels.size(0)
 
-                remaining = self.num_images - self._features.size(0)
+                if remaining > label.size(0):
+                    remaining = label.size(0)
 
-                if remaining > feature.size(0):
-                    remaining = feature.size(0)
-
-                self._features = torch.cat((self._features, feature[:remaining].cpu()), dim=0)
                 self._images = torch.cat((self._images, x[:remaining].cpu()), dim=0)
                 self._labels = torch.cat((self._labels, label[:remaining].cpu()), dim=0)
 
-            if self._features.size(0) >= self.num_images:
-                self._writer.add_embedding(self._features, metadata=self._labels, label_img=self._images)
+                if data is not None:
+                    self._data = torch.cat((self._data, data[:remaining].cpu()), dim=0)
+
+                if feature is not None:
+                    self._features = torch.cat((self._features, feature[:remaining].cpu()), dim=0)
+
+            if self._labels.size(0) >= self.num_images:
+                if state['epoch'] == 0 and self.write_data:
+                    self._writer.add_embedding(self._data, metadata=self._labels, label_img=self._images, tag='data', global_step=-1)
+                if self.write_features:
+                    self._writer.add_embedding(self._features, metadata=self._labels, label_img=self._images, tag='features', global_step=state['epoch'])
                 self.done = True
+
+    def on_end_epoch(self, state):
+        if self.write_features:
+            self.done = False
 
     def on_end(self, state):
         self._writer.close()
