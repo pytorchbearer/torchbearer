28fbe0a6dd45140af2c28a2a0aa3deb60d9dce3d
Ethan Harris <ewah1g13@soton.ac.uk>
2018-03-29 10:42:30 +0100

    Add tensorboard data vis calback! (#79)

--- Example/example.py
+++ Example/example.py
@@ -9,7 +9,7 @@ from torch.utils.data import DataLoader
 from Example import inception_network as nm
 from bink.bink import Model
 from bink.metrics import RocAucScore
-from bink.callbacks import EarlyStopping, TensorBoard
+from bink.callbacks import EarlyStopping, TensorBoard, TensorBoardImageVis
 
 
 ####### Paths #######
@@ -27,8 +27,8 @@ transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-trainset = torchvision.datasets.MNIST(dataset_path, train=True, transform=transform)
-testset =  torchvision.datasets.MNIST(dataset_path, train=False, transform=transform)
+trainset = torchvision.datasets.CIFAR10(dataset_path, train=True, transform=transform)
+testset =  torchvision.datasets.CIFAR10(dataset_path, train=False, transform=transform)
 
 trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
 testloader = DataLoader(testset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
@@ -41,9 +41,12 @@ modelPath = os.getcwd() + '/' + modelName + '/' + folderName + '/'
 
 
 
-model = nm.InceptionSmall()
+model = nm.InceptionSmall(channels=3)
 
 ####### Trainer #######
 
+from datetime import datetime
+current_time = datetime.now().strftime('%b%d_%H-%M-%S')
+
 model = Model(model, torch.optim.SGD(model.parameters(), 0.001), nn.CrossEntropyLoss(), metrics=['acc', 'loss']).cuda()
-model.fit_generator(trainloader, validation_generator=testloader, callbacks=[TensorBoard(write_batch_metrics=True, write_graph=True)], epochs=100)
+model.fit_generator(trainloader, validation_generator=testloader, callbacks=[TensorBoardImageVis(num_images=250, comment=current_time), TensorBoard(comment=current_time, write_batch_metrics=True, write_graph=True)], epochs=100)
