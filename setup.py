d04779ef8fd686fa999a2bc0ae240dd920fa56ca
Ethan Harris <ethanwharris@gmail.com>
2018-05-24 14:54:34 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='bink',
+    version='0.1.0',
+    packages=['bink', 'bink.metrics', 'bink.runners', 'bink.callbacks', 'tests', 'tests.metrics', 'tests.callbacks'],
+    url='https://github.com/MattPainter01/PyTorch-bink',
+    license='GPL-3.0',
+    author='Matt Painter',
+    author_email='mp2u16@ecs.soton.ac.uk',
+    description='A model training library for pytorch',
+    install_requires=['numpy', 'torch>=0.4', 'scikit-learn', 'tqdm', 'tensorboardX>=1.2'],
+    python_requires='>=3',
+)
