051086eabebf4a2ccc2e38e66a04c9e71082bcda
Ethan Harris <ethanwharris@gmail.com>
2018-05-24 15:33:13 +0100

    Add setup cfg and make changes for upload to PyPi

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.1.0',
     packages=['bink', 'bink.metrics', 'bink.runners', 'bink.callbacks', 'tests', 'tests.metrics', 'tests.callbacks'],
     url='https://github.com/MattPainter01/PyTorch-bink',
+    download_url='https://github.com/MattPainter01/PyTorch-bink/archive/v0.1.0.tar.gz',
     license='GPL-3.0',
     author='Matt Painter',
     author_email='mp2u16@ecs.soton.ac.uk',
