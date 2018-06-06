e0b64a0ab3d0be3bb9b1afdd85256a21437abea2
Ethan Harris <ewah1g13@soton.ac.uk>
2018-06-06 15:13:55 +0100

    Update LR schedulers (#134)

--- bink/callbacks/torch_scheduler.py
+++ bink/callbacks/torch_scheduler.py
@@ -4,9 +4,13 @@ import torch
 
 
 class TorchScheduler(Callback):
-    def __init__(self, scheduler, monitor=None):
-        self._scheduler = scheduler
+    def __init__(self, scheduler_builder, monitor=None):
+        self._scheduler_builder = scheduler_builder
         self._monitor = monitor
+        self._scheduler = None
+
+    def on_start(self, state):
+        self._scheduler = self._scheduler_builder(state['optimizer'])
 
     def on_start_training(self, state):
         if self._monitor is None:
@@ -22,8 +26,9 @@ class LambdaLR(TorchScheduler):
     See:
         `PyTorch LambdaLR <http://pytorch.org/docs/master/optim.html#torch.optim.lr_scheduler.LambdaLR>`_
     """
-    def __init__(self, *args, **kwargs):
-        super(LambdaLR, self).__init__(torch.optim.lr_scheduler.LambdaLR(*args, **kwargs))
+    def __init__(self, lr_lambda, last_epoch=-1):
+        super(LambdaLR, self).__init__(lambda opt:
+                                       torch.optim.lr_scheduler.LambdaLR(opt, lr_lambda, last_epoch=last_epoch))
 
 
 class StepLR(TorchScheduler):
@@ -31,8 +36,10 @@ class StepLR(TorchScheduler):
     See:
         `PyTorch StepLR <http://pytorch.org/docs/master/optim.html#torch.optim.lr_scheduler.StepLR>`_
     """
-    def __init__(self, *args, **kwargs):
-        super(StepLR, self).__init__(torch.optim.lr_scheduler.StepLR(*args, **kwargs))
+    def __init__(self, step_size, gamma=0.1, last_epoch=-1):
+        super(StepLR, self).__init__(lambda opt:
+                                     torch.optim.lr_scheduler.StepLR(opt, step_size, gamma=gamma,
+                                                                     last_epoch=last_epoch))
 
 
 class MultiStepLR(TorchScheduler):
@@ -40,8 +47,10 @@ class MultiStepLR(TorchScheduler):
     See:
         `PyTorch MultiStepLR <http://pytorch.org/docs/master/optim.html#torch.optim.lr_scheduler.MultiStepLR>`_
     """
-    def __init__(self, *args, **kwargs):
-        super(MultiStepLR, self).__init__(torch.optim.lr_scheduler.MultiStepLR(*args, **kwargs))
+    def __init__(self, milestones, gamma=0.1, last_epoch=-1):
+        super(MultiStepLR, self).__init__(lambda opt:
+                                          torch.optim.lr_scheduler.MultiStepLR(opt, milestones, gamma=gamma,
+                                                                               last_epoch=last_epoch))
 
 
 class ExponentialLR(TorchScheduler):
@@ -49,8 +58,9 @@ class ExponentialLR(TorchScheduler):
     See:
         `PyTorch ExponentialLR <http://pytorch.org/docs/master/optim.html#torch.optim.lr_scheduler.ExponentialLR>`_
     """
-    def __init__(self, *args, **kwargs):
-        super(ExponentialLR, self).__init__(torch.optim.lr_scheduler.ExponentialLR(*args, **kwargs))
+    def __init__(self, gamma, last_epoch=-1):
+        super(ExponentialLR, self).__init__(lambda opt:
+                                            torch.optim.lr_scheduler.ExponentialLR(opt, gamma, last_epoch=last_epoch))
 
 
 class CosineAnnealingLR(TorchScheduler):
@@ -58,8 +68,10 @@ class CosineAnnealingLR(TorchScheduler):
     See:
         `PyTorch CosineAnnealingLR <http://pytorch.org/docs/master/optim.html#torch.optim.lr_scheduler.CosineAnnealingLR>`_
     """
-    def __init__(self, *args, **kwargs):
-        super(CosineAnnealingLR, self).__init__(torch.optim.lr_scheduler.CosineAnnealingLR(*args, **kwargs))
+    def __init__(self, T_max, eta_min=0, last_epoch=-1):
+        super(CosineAnnealingLR, self).__init__(lambda opt:
+                                                torch.optim.lr_scheduler.CosineAnnealingLR(opt, T_max, eta_min=eta_min,
+                                                                                           last_epoch=last_epoch))
 
 
 class ReduceLROnPlateau(TorchScheduler):
@@ -69,5 +81,10 @@ class ReduceLROnPlateau(TorchScheduler):
     See:
         `PyTorch ReduceLROnPlateau <http://pytorch.org/docs/master/optim.html#torch.optim.lr_scheduler.ReduceLROnPlateau>`_
     """
-    def __init__(self, *args, monitor='val_loss', **kwargs):
-        super(ReduceLROnPlateau, self).__init__(torch.optim.lr_scheduler.ReduceLROnPlateau(*args, **kwargs), monitor=monitor)
+    def __init__(self,  monitor='val_loss', mode='min', factor=0.1, patience=10, verbose=False, threshold=1e-4,
+                 threshold_mode='rel', cooldown=0, min_lr=0, eps=1e-8):
+        super(ReduceLROnPlateau, self).__init__(lambda opt:
+                                                torch.optim.lr_scheduler.ReduceLROnPlateau(
+                                                    opt, mode=mode, factor=factor, patience=patience, verbose=verbose,
+                                                    threshold=threshold, threshold_mode=threshold_mode,
+                                                    cooldown=cooldown, min_lr=min_lr, eps=eps), monitor=monitor)
