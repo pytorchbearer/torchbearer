58273cb9140cd3d8b3813fd52af05fed1fce0a40
Ethan Harris <ewah1g13@soton.ac.uk>
2018-06-08 16:12:11 +0100

    Bump version number (#142)

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='bink',
-    version='0.1.1',
+    version='0.1.2',
     packages=['bink', 'bink.metrics', 'bink.runners', 'bink.callbacks', 'tests', 'tests.metrics', 'tests.callbacks'],
     url='https://github.com/MattPainter01/PyTorch-bink',
-    download_url='https://github.com/MattPainter01/PyTorch-bink/archive/v0.1.0.tar.gz',
+    download_url='https://github.com/MattPainter01/PyTorch-bink/archive/v0.1.2.tar.gz',
     license='GPL-3.0',
     author='Matt Painter',
     author_email='mp2u16@ecs.soton.ac.uk',
