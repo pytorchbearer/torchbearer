23c9c1e0520431e80c09064de5c17fa9ee1c1cea
Ethan Harris <ewah1g13@soton.ac.uk>
2018-06-09 19:24:01 +0100

    Comment defaults.py

--- bink/metrics/defaults.py
+++ bink/metrics/defaults.py
@@ -2,10 +2,20 @@ from bink import metrics
 
 
 def loss():
+    """Return the default loss metric, running / overall mean and std.
+
+    :return: Metric -- Running and final loss metrics.
+
+    """
     return metrics.MetricList([metrics.running_stats(metrics.loss_primitive), metrics.stats(metrics.loss_primitive)])
 
 
 def accuracy():
+    """Return the default accuracy metric, running / overall mean and std of categorical accuracy.
+
+    :return: Metric -- Running and final categorical accuracy metrics.
+
+    """
     return metrics.MetricList([metrics.running_stats(metrics.categorical_accuracy_primitive), metrics.stats(metrics.categorical_accuracy_primitive)])
 
 
