171604605e737dc148a2cd76859a72c519cee1ba
Ethan Harris <ethanwharris@gmail.com>
2018-07-12 07:33:28 +0100

    Document csv_logger.py

--- bink/callbacks/csv_logger.py
+++ bink/callbacks/csv_logger.py
@@ -3,7 +3,23 @@ import csv
 
 
 class CSVLogger(Callback):
+    """Callback to log metrics to a csv file.
+    """
+
     def __init__(self, filename, separator=',', batch_granularity=False, write_header=True, append=False):
+        """Construct a CSVLogger callback which logs metrics to the given file.
+
+        :param filename: The name of the file to output to
+        :type filename: str
+        :param separator: The delimiter to use (e.g. comma, tab etc.)
+        :type separator: str
+        :param batch_granularity: If True, write on each batch, else on each epoch
+        :type batch_granularity: bool
+        :param write_header: If True, write the CSV header at the beginning of training
+        :type write_header: bool
+        :param append: If True, append to the file instead of replacing it
+        :type append: bool
+        """
         super().__init__()
         self.batch_granularity = batch_granularity
         self.filename = filename
