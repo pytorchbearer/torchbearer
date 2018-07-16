98385244fb65771854450a17e358c065aea9941e
Ethan Harris <ewah1g13@soton.ac.uk>
2018-07-16 17:57:54 +0100

    Update conf.py (#192)

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ class Mock(MagicMock):
         return MagicMock()
 
 
-MOCK_MODULES = ['torch', 'numpy', 'sklearn', 'tqdm', 'tensorboardX']
+MOCK_MODULES = ['torch', 'numpy', 'sklearn', 'sklearn.metrics', 'tqdm', 'tensorboardX']
 
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
