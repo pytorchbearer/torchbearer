caa5a7ee5694aa9b4c62dd3c46997b6f4ccbd46a
Ethan Harris <ethanwharris@gmail.com>
2018-07-16 17:05:04 +0100

    Mock required libraries for docs build

--- docs/conf.py
+++ docs/conf.py
@@ -19,6 +19,18 @@
 #
 import os
 import sys
+from unittest.mock import MagicMock
+
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+
+MOCK_MODULES = ['torch', 'numpy', 'scikit-learn', 'tqdm', 'tensorboardX']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 sys.path.insert(0, os.path.abspath('../'))
 
 
