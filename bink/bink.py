59504e555fa9e54e8258534f7d595cc693a41b83
Matt Painter <mp2u16@ecs.soton.ac.uk>
2018-07-16 13:48:25 +0100

    Test/bink (#181)
    * Initial bink tests
    
    * initial bink test
    
    * Add tests for bink.py

--- bink/bink.py
+++ bink/bink.py
@@ -1,3 +1,7 @@
+import math
+import warnings
+from _warnings import warn
+
 import torch
 from torch.utils.data import DataLoader, TensorDataset
 
@@ -10,10 +14,10 @@ from bink import metrics as bink_metrics
 
 
 class Model:
-    ''' Binkmodel to wrap base torch model and provide training environment around it
-    '''
+    """ Binkmodel to wrap base torch model and provide training environment around it
+    """
     def __init__(self, model, optimizer, loss_criterion, metrics=[]):
-        ''' Create binkmodel which wraps a base torchmodel and provides a training environment surrounding it
+        """ Create binkmodel which wraps a base torchmodel and provides a training environment surrounding it
 
         :param model: The base pytorch model
         :type model: torch.nn.Module
@@ -23,7 +27,7 @@ class Model:
         :type loss_criterion: function
         :param metrics: Additional metrics for display and use within callbacks
         :type metrics: list
-        '''
+        """
         super().__init__()
         self.main_state = {
             bink.MODEL: model,
@@ -38,7 +42,7 @@ class Model:
     def fit(self, x, y, batch_size=None, epochs=1, verbose=1, callbacks=[], validation_split=0.0,
             validation_data=None, shuffle=True, initial_epoch=0,
             steps_per_epoch=None, validation_steps=None, workers=1, pass_state=False):
-        ''' Perform fitting of a model to given data and label tensors
+        """ Perform fitting of a model to given data and label tensors
 
         :param x: The input data tensor
         :type x: torch.Tensor
@@ -70,7 +74,7 @@ class Model:
         :type pass_state: bool
         :return: The final state context dictionary
         :rtype: dict[str,any]
-        '''
+        """
         
         trainset, valset = get_train_valid_sets(x, y, validation_data, validation_split, shuffle=shuffle)
         trainloader = DataLoader(trainset, batch_size, shuffle=shuffle, num_workers=workers)
@@ -86,7 +90,7 @@ class Model:
 
     def fit_generator(self, generator, train_steps=None, epochs=1, verbose=1, callbacks=[],
                       validation_generator=None, validation_steps=None, initial_epoch=0, pass_state=False):
-        ''' Perform fitting of a model to given data generator
+        """ Perform fitting of a model to given data generator
 
         :param generator: The training data generator (usually a pytorch DataLoader)
         :type generator: DataLoader
@@ -108,7 +112,7 @@ class Model:
         :type pass_state: bool
         :return: The final state context dictionary
         :rtype: dict[str,any]
-        '''
+        """
         if verbose == 1:
             callbacks = [Tqdm()] + callbacks
         _callbacks = CallbackList(callbacks)
@@ -116,8 +120,19 @@ class Model:
         # Get train and validation steps
         if validation_steps is None and validation_generator is not None:
             validation_steps = len(validation_generator)
-        if train_steps is None:
+        if train_steps is None or train_steps > len(generator):
             train_steps = len(generator)
+        if not isinstance(train_steps, int):
+            train_steps = int(train_steps)
+            warnings.warn("Number of training steps is not an int, converting to int")
+
+        if not isinstance(epochs, int):
+            if isinstance(epochs, float):
+                epochs = int(epochs)
+                warnings.warn("Number of epochs is a float, converting to int")
+            else:
+                warnings.warn("Number of epochs is neither float nor int, setting to 0")
+                epochs = 0
 
         # Init state
         state = {
@@ -196,7 +211,7 @@ class Model:
         return state
 
     def _test_loop(self, state, callbacks, pass_state, batch_loader, num_steps=None):
-        ''' The generic testing loop used for validation, evaluation and prediction
+        """ The generic testing loop used for validation, evaluation and prediction
 
         :param state: The current state context dictionary
         :type state: dict[str,any]
@@ -204,25 +219,29 @@ class Model:
         :type callbacks: CallbackList
         :param pass_state: If True the state dictionary is passed to the torch model forward method, if False only the input data is passed
         :type pass_state: bool
-        :param batch_loader: The type of batch loader to use
+        :param batch_loader: The batch loader to use
         :type batch_loader: function
         :param num_steps: The number of testing mini-batches to run
         :return: The state context dictionary
         :rtype: dict[str,any]
-        '''
+        """
         with torch.no_grad():
             state[bink.METRIC_LIST].reset(state)
             state[bink.METRICS] = {}
 
-            if num_steps is None:
+
+            if num_steps is None or num_steps > len(state[bink.VALIDATION_GENERATOR]):
                 num_steps = len(state[bink.VALIDATION_GENERATOR])
+            if not isinstance(num_steps, int):
+                num_steps = int(num_steps)
+                warnings.warn('Num test steps is not an int, converting to int.', Warning)
 
             state[bink.VALIDATION_STEPS] = num_steps
             state[bink.VALIDATION_ITERATOR] = iter(state[bink.VALIDATION_GENERATOR])
 
             callbacks.on_start_validation(state)
 
-            for state[bink.BATCH] in range(num_steps):
+            for state[bink.BATCH] in range(state[bink.VALIDATION_STEPS]):
                 # Load batch
                 batch_loader('validation', state)
                 callbacks.on_sample_validation(state)
@@ -250,7 +269,7 @@ class Model:
         return state
 
     def _validate(self, state, _callbacks, pass_state):
-        ''' Perform a validation loop
+        """ Perform a validation loop
 
         :param state: The current context state dictionary
         :param _callbacks: The list of bink callbacks to be called during validation loop
@@ -259,11 +278,11 @@ class Model:
         :type pass_state: bool
         :return: None
         :rtype: None
-        '''
+        """
         self._test_loop(state, _callbacks, pass_state, self._load_batch_standard, state[bink.VALIDATION_STEPS])
 
     def evaluate(self, x=None, y=None, batch_size=32, verbose=1, steps=None, pass_state=False):
-        ''' Perform an evaluation loop on given data and label tensors to evaluate metrics
+        """ Perform an evaluation loop on given data and label tensors to evaluate metrics
 
         :param x: The input data tensor
         :type x: torch.Tensor
@@ -279,12 +298,12 @@ class Model:
         :type pass_state: bool
         :return: The dictionary containing final metrics
         :rtype: dict[str,any]
-        '''
+        """
         trainset = DataLoader(TensorDataset(x, y), batch_size, steps)
         return self.evaluate_generator(trainset, verbose, pass_state=pass_state)
 
     def evaluate_generator(self, generator, verbose=1, steps=None, pass_state=False):
-        ''' Perform an evaluation loop on given data generator to evaluate metrics
+        """ Perform an evaluation loop on given data generator to evaluate metrics
 
         :param generator: The evaluation data generator (usually a pytorch DataLoader)
         :type generator: DataLoader
@@ -296,7 +315,7 @@ class Model:
         :type pass_state: bool
         :return: The dictionary containing final metrics
         :rtype: dict[str,any]
-        '''
+        """
 
         state = {bink.EPOCH: 0, bink.MAX_EPOCHS: 1, bink.STOP_TRAINING: False, bink.VALIDATION_GENERATOR: generator}
         state.update(self.main_state)
@@ -309,7 +328,7 @@ class Model:
         return state[bink.METRICS]
 
     def predict(self, x=None, batch_size=None, verbose=1, steps=None, pass_state=False):
-        ''' Perform a prediction loop on given data tensor to predict labels
+        """ Perform a prediction loop on given data tensor to predict labels
 
         :param x: The input data tensor
         :type x: torch.Tensor
@@ -323,12 +342,12 @@ class Model:
         :type pass_state: bool
         :return: Tensor of final predicted labels
         :rtype: torch.Tensor
-        '''
+        """
         pred_set = DataLoader(TensorDataset(x, None), batch_size, steps)
         return self.predict_generator(pred_set, verbose, pass_state=pass_state)
 
     def predict_generator(self, generator, verbose=1, steps=None, pass_state=False):
-        '''Perform a prediction loop on given data generator to predict labels
+        """Perform a prediction loop on given data generator to predict labels
 
         :param generator: The prediction data generator (usually a pytorch DataLoader)
         :type generator: DataLoader
@@ -340,7 +359,7 @@ class Model:
         :type pass_state: bool
         :return: Tensor of final predicted labels
         :rtype: torch.Tensor
-        '''
+        """
         state = {bink.EPOCH: 0, bink.MAX_EPOCHS: 1, bink.STOP_TRAINING: False, bink.VALIDATION_GENERATOR: generator}
         state.update(self.main_state)
 
@@ -352,25 +371,25 @@ class Model:
         return state[bink.FINAL_PREDICTIONS]
 
     def train(self):
-        ''' Set model and metrics to training mode
-        '''
+        """ Set model and metrics to training mode
+        """
         self.main_state[bink.MODEL].train()
         self.main_state[bink.METRIC_LIST].train()
 
     def eval(self):
-        ''' Set model and metrics to evaluation mode
-        '''
+        """ Set model and metrics to evaluation mode
+        """
         self.main_state[bink.MODEL].eval()
         self.main_state[bink.METRIC_LIST].eval()
 
     def to(self, *args, **kwargs):
-        ''' Moves and/or casts the parameters and buffers.
+        """ Moves and/or casts the parameters and buffers.
 
         :param args: See `torch.nn.Module.to https://pytorch.org/docs/stable/nn.html?highlight=#torch.nn.Module.to`
         :param kwargs: See `torch.nn.Module.to https://pytorch.org/docs/stable/nn.html?highlight=#torch.nn.Module.to`
         :return: Self binkmodel
         :rtype: Model
-        '''
+        """
         self.main_state[bink.MODEL].to(*args, **kwargs)
 
         for state in self.main_state[bink.OPTIMIZER].state.values():
@@ -378,54 +397,45 @@ class Model:
                 if torch.is_tensor(v):
                     state[k] = v.to(*args, **kwargs)
 
-        for key, val in kwargs.items():
-            if key == bink.DATA_TYPE:
-                self.main_state[bink.DATA_TYPE] = kwargs[bink.DATA_TYPE]
-            elif bink.DEVICE in kwargs:
-                self.main_state[bink.DEVICE] = kwargs[bink.DEVICE]
-        for arg in args:
-            if isinstance(arg, torch.dtype):
-                self.main_state[bink.DATA_TYPE] = arg
-            else:
-                self.main_state[bink.DEVICE] = arg
+        self.main_state = Model._update_device_and_dtype_from_args(self.main_state, *args, **kwargs)
 
         return self
 
     def cuda(self, device=torch.cuda.current_device()):
-        ''' Moves all model parameters and buffers to the GPU.
+        """ Moves all model parameters and buffers to the GPU.
 
         :param device: if specified, all parameters will be copied to that device
         :type device: int, optional
         :return: Self binkmodel
         :rtype: Model
-        '''
+        """
         return self.to('cuda:' + str(device))
 
     def cpu(self):
-        ''' Moves all model parameters and buffers to the CPU.
+        """ Moves all model parameters and buffers to the CPU.
 
         :return: Self binkmodel
         :rtype: Model
-        '''
+        """
         return self.to('cpu')
 
     def load_state_dict(self, state_dict, **kwargs):
-        ''' Copies parameters and buffers from :func:`state_dict` into this module and its descendants.
+        """ Copies parameters and buffers from :func:`state_dict` into this module and its descendants.
 
         :param state_dict: A dict containing parameters and persistent buffers.
         :type state_dict: dict
         :param kwargs: `See torch.nn.Module.load_state_dict https://pytorch.org/docs/stable/nn.html?highlight=#torch.nn.Module.load_state_dict`
-        '''
+        """
         self.main_state[bink.MODEL].load_state_dict(state_dict[bink.MODEL], **kwargs)
         self.main_state[bink.OPTIMIZER].load_state_dict(state_dict[bink.OPTIMIZER])
 
     def state_dict(self, **kwargs):
-        '''
+        """
         :param kwargs: See `torch.nn.Module.state_dict https://pytorch.org/docs/stable/nn.html?highlight=#torch.nn.Module.state_dict`
 
         :return: A dict containing parameters and persistent buffers.
         :rtype: dict
-        '''
+        """
         state_dict = {
             bink.MODEL: self.main_state[bink.MODEL].state_dict(**kwargs),
             bink.OPTIMIZER: self.main_state[bink.OPTIMIZER].state_dict()
@@ -434,7 +444,7 @@ class Model:
 
     @staticmethod
     def _deep_to(batch, device, dtype):
-        ''' Static method to call :func:`to` on tensors or tuples. All items in tuple will have :func:_deep_to called
+        """ Static method to call :func:`to` on tensors or tuples. All items in tuple will have :func:_deep_to called
 
         :param batch: The mini-batch which requires a :func:`to` call
         :type batch: tuple, list, torch.Tensor
@@ -444,7 +454,7 @@ class Model:
         :type dtype: torch.dtype
         :return: The moved or casted batch
         :rtype: tuple, list, torch.Tensor
-        '''
+        """
         is_tuple = isinstance(batch, tuple)
 
         if isinstance(batch, list) or isinstance(batch, tuple):
@@ -462,26 +472,51 @@ class Model:
 
     @staticmethod
     def _load_batch_standard(iterator, state):
-        ''' Static method to load a standard (input data, target) tuple mini-batch from iterator into state
+        """ Static method to load a standard (input data, target) tuple mini-batch from iterator into state
 
         :param iterator: Training or validation data iterator
         :type iterator: iterable
         :param state: The current state dict of the :class:`Model`.
         :type state: dict[str,any]
-        '''
+        """
         state[bink.X], state[bink.Y_TRUE] = Model._deep_to(next(state[iterator + '_iterator']), state[bink.DEVICE], state[bink.DATA_TYPE])
 
     @staticmethod
     def _load_batch_predict(iterator, state):
-        ''' Static method to load a prediction (input data, target) or (input data) mini-batch from iterator into state
+        """ Static method to load a prediction (input data, target) or (input data) mini-batch from iterator into state
 
         :param iterator: Training or validation data iterator
         :type iterator: iterable
         :param state: The current state dict of the :class:`Model`.
         :type state: dict[str,any]
-        '''
+        """
         data = Model._deep_to(next(state[iterator + '_iterator']), state[bink.DEVICE], state[bink.DATA_TYPE])
         if isinstance(data, list) or isinstance(data, tuple):
             state[bink.X], state[bink.Y_TRUE] = data
         else:
             state[bink.X] = data
+
+    @staticmethod
+    def _update_device_and_dtype_from_args(main_state, *args, **kwargs):
+        """ static method to update a main state dictionary with new data type and device values
+        
+        :param main_state: The main state to update
+        :type main_state: dict[str,any]
+        :param args: Arguments to the :func:`Model.to` function
+        :param kwargs: Keyword arguments to the :func:`Model.to` function
+        :return: Updated main state dictionary
+        :rtype: dict[str,any]
+        """
+        for key, val in kwargs.items():
+            if key == bink.DATA_TYPE:
+                main_state[bink.DATA_TYPE] = kwargs[bink.DATA_TYPE]
+            elif bink.DEVICE in kwargs:
+                main_state[bink.DEVICE] = kwargs[bink.DEVICE]
+
+        for arg in args:
+            if isinstance(arg, torch.dtype):
+                main_state[bink.DATA_TYPE] = arg
+            else:
+                main_state[bink.DEVICE] = arg
+
+        return main_state
