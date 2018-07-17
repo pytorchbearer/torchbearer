0fa16256448eb23d002450bd4b45e11f0045151a
Ethan Harris <ethanwharris@gmail.com>
2018-07-17 11:41:45 +0100

    Mock torch.cuda in tests

---
+++ tests/callbacks/__init__.py
@@ -0,0 +1,13 @@
+import sys
+from unittest.mock import MagicMock
+
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+
+MOCK_MODULES = ['torch.cuda']
+
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
