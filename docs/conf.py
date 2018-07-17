a62fbee57084a0f163b9f47581d9a846a5ef61c8
Ethan Harris <ethanwharris@gmail.com>
2018-07-17 10:58:27 +0100

    Add / update quickstart example

--- docs/conf.py
+++ docs/conf.py
@@ -102,7 +102,10 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'collapse_navigation': False,
+    'display_version': True
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
