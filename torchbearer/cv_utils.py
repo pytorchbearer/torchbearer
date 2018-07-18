c7562c5a812b930b00e659b68510bfe0ed027e2f
Matt Painter <mp2u16@ecs.soton.ac.uk>
2018-07-18 16:38:48 +0100

    Test/cvutils (#212)
    * Add tests for cv utils
    
    * Add additional test

--- torchbearer/cv_utils.py
+++ torchbearer/cv_utils.py
@@ -1,10 +1,25 @@
+import math
+
 import torch
 from torch.utils.data import TensorDataset
 
 
 def train_valid_splitter(x, y, split, shuffle=True):
-    num_samples_x = x.shape[0]
-    num_valid_samples = torch.floor(num_samples_x * split)
+    ''' Generate training and validation tensors from whole dataset data and label tensors
+    
+    :param x: Data tensor for whole dataset
+    :type x: torch.Tensor
+    :param y: Label tensor for whole dataset
+    :type y: torch.Tensor
+    :param split: Fraction of dataset to be used for validation
+    :type split: float
+    :param shuffle: If True randomize tensor order before splitting else do not randomize 
+    :type shuffle: bool
+    :return: Training and validation tensors (training data, training labels, validation data, validation labels)
+    :rtype: tuple
+    '''
+    num_samples_x = x.size()[0]
+    num_valid_samples = math.floor(num_samples_x * split)
 
     if shuffle:
         indicies = torch.randperm(num_samples_x)
@@ -17,12 +32,27 @@ def train_valid_splitter(x, y, split, shuffle=True):
 
 
 def get_train_valid_sets(x, y, validation_data, validation_split, shuffle=True):
+    ''' Generate validation and training datasets from whole dataset tensors
+    
+    :param x: Data tensor for dataset
+    :type x: torch.Tensor
+    :param y: Label tensor for dataset
+    :type y: torch.Tensor
+    :param validation_data: Optional validation data (x_val, y_val) to be used instead of splitting x and y tensors 
+    :type validation_data: (torch.Tensor, torch.Tensor)
+    :param validation_split: Fraction of dataset to be used for validation
+    :type validation_split: float
+    :param shuffle: If True randomize tensor order before splitting else do not randomize 
+    :type shuffle: bool
+    :return: Training and validation datasets
+    :rtype: tuple
+    '''
 
     valset = None
 
     if validation_data is not None:
         x_val, y_val = validation_data
-    elif validation_split > 0.0:
+    elif isinstance(validation_split, float):
         x, y, x_val, y_val = train_valid_splitter(x, y, validation_split, shuffle=shuffle)
     else:
         x_val, y_val = None, None
