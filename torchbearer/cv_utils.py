7c5d6d376f1bf590991a32ef33abce6198930900
Matt <mp2u16@soton.ac.uk>
2018-07-24 12:23:34 +0100

    Commenting

--- torchbearer/cv_utils.py
+++ torchbearer/cv_utils.py
@@ -67,6 +67,13 @@ def get_train_valid_sets(x, y, validation_data, validation_split, shuffle=True):
 
 class DatasetValidationSplitter:
     def __init__(self, dataset_len, split_fraction, shuffle_seed=None):
+        ''' Generates training and validation split indicies for a given dataset length and creates training and
+        validation datasets using these splits
+
+        :param dataset_len: The length of the dataset to be split into training and validation
+        :param split_fraction: The fraction of the whole dataset to be used for validation
+        :param shuffle_seed: Optional random seed for the shuffling process
+        '''
         super().__init__()
         self.dataset_len = dataset_len
         self.split_fraction = split_fraction
@@ -86,14 +93,35 @@ class DatasetValidationSplitter:
         self.train_ids = all_ids[num_valid_ids:]
 
     def get_train_dataset(self, dataset):
+        ''' Creates a training dataset from existing dataset
+
+        :param dataset: Dataset to be split into a training dataset
+        :type dataset: torch.utils.data.Dataset
+        :return: Training dataset split from whole dataset
+        :rtype: torch.utils.data.Dataset
+        '''
         return SubsetDataset(dataset, self.train_ids)
 
     def get_val_dataset(self, dataset):
+        ''' Creates a validation dataset from existing dataset
+
+        :param dataset: Dataset to be split into a validation dataset
+        :type dataset: torch.utils.data.Dataset
+        :return: Validation dataset split from whole dataset
+        :rtype: torch.utils.data.Dataset
+        '''
         return SubsetDataset(dataset, self.valid_ids)
 
 
 class SubsetDataset(Dataset):
     def __init__(self, dataset, ids):
+        ''' Dataset that consists of a subset of a previous dataset
+
+        :param dataset: Complete dataset
+        :type dataset: torch.utils.data.Dataset
+        :param ids: List of subset IDs
+        :type ids: list
+        '''
         super().__init__()
         self.dataset = dataset
         self.ids = ids
