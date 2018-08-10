ec9f6db22247ea96f05241787f91b4d9cdf2260e
Ethan Harris <ethanwharris@gmail.com>
2018-08-10 09:43:05 +0100

    Move version number to one place

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup
 
+version_dict = {}
+exec(open("./torchbearer/version.py").read(), version_dict)
+
 setup(
     name='torchbearer',
-    version='0.1.5',
+    version=version_dict['__version__'],
     packages=['torchbearer', 'torchbearer.metrics', 'torchbearer.callbacks', 'tests', 'tests.metrics', 'tests.callbacks'],
     url='https://github.com/ecs-vlc/torchbearer',
-    download_url='https://github.com/ecs-vlc/torchbearer/archive/0.1.5.tar.gz',
+    download_url='https://github.com/ecs-vlc/torchbearer/archive/' + version_dict['__version__'] + '.tar.gz',
     license='GPL-3.0',
     author='Matt Painter',
     author_email='mp2u16@ecs.soton.ac.uk',
