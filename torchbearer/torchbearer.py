7f1bba8dd36807cf71e2725306b83c5f7e42e988
Matt <mp2u16@soton.ac.uk>
2018-08-13 11:13:52 +0100

    Make new callback list object each fit call

--- torchbearer/torchbearer.py
+++ torchbearer/torchbearer.py
@@ -142,6 +142,7 @@ class Model:
             torchbearer.STOP_TRAINING: False
         }
         state.update(self.main_state)
+        state[torchbearer.CALLBACK_LIST] = CallbackList(state[torchbearer.CALLBACK_LIST].callback_list)
         state[torchbearer.CALLBACK_LIST].append(_callbacks)
 
         state[torchbearer.CALLBACK_LIST].on_start(state)
