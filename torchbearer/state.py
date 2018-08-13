05a0a298897a6af5267dfef249f8312d74e3f763
Matt <mp2u16@soton.ac.uk>
2018-08-13 18:49:57 +0100

    Add State, Statekey and @from_state

--- torchbearer/state.py
+++ torchbearer/state.py
@@ -1,4 +1,6 @@
-STATE_KEYS = []
+import warnings
+
+__keys__ = []
 
 
 def state_key(key):
@@ -9,18 +11,61 @@ def state_key(key):
     :return: New state key
     :rtype: String
     """
-    if key in STATE_KEYS:
-        count = 1
-        my_key = key + '_' + str(count)
+    return Statekey(key)
 
-        while my_key in STATE_KEYS:
-            count += 1
+
+class Statekey:
+    def __init__(self, key):
+        super().__init__()
+        self.key = self._gen_key_(key)
+
+    def _gen_key_(self, key):
+        if key in __keys__:
+            count = 1
             my_key = key + '_' + str(count)
 
-        key = my_key
+            while my_key in __keys__:
+                count += 1
+                my_key = key + '_' + str(count)
 
-    STATE_KEYS.append(key)
-    return key
+            key = my_key
+
+        __keys__.append(key)
+        return key
+
+    def __repr__(self):
+        return self.key
+
+    def __str__(self):
+        return self.key
+
+
+class State(dict):
+    def __init__(self):
+        super().__init__()
+
+    def get_key(self, statekey):
+        if isinstance(statekey, str):
+            warnings.warn("State was accessed with a string, generate keys with Statekey(str).")
+        return str(statekey)
+
+    def __getitem__(self, key):
+        return super().__getitem__(self.get_key(key))
+
+    def __setitem__(self, key, val):
+        super().__setitem__(self.get_key(key), val)
+
+    def __delitem__(self, val):
+        super().__delitem__(val)
+
+    def __contains__(self, o: object) -> bool:
+        return super().__contains__(self.get_key(o))
+
+    def update(self, d):
+        new_dict = {}
+        for key in d:
+            new_dict[self.get_key(key)] = d[key]
+        super().update(new_dict)
 
 
 MODEL = state_key('model')
@@ -48,3 +93,15 @@ FINAL_PREDICTIONS = state_key('final_predictions')
 BATCH = state_key('t')
 TIMINGS = state_key('timings')
 CALLBACK_LIST = state_key('callback_list')
+
+
+# d = {CALLBACK_LIST: 'test_update', MODEL: '123'}
+# s = State()
+# s[MODEL] = 'test'
+# m1=state_key('model')
+# s[m1] = 'two'
+# s[state_key('model')] = 'three'
+# s['ttt'] = 't'
+# s.update(d)
+# print(s)
+# print(len(s))
