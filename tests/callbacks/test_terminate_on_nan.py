1983d00b0e8d9658c155a9342a1ee1dc0c10dd69
Matt Painter <mp2u16@ecs.soton.ac.uk>
2018-08-15 09:58:30 +0100

    Trial api/torchbearer tests (#328)
    * Fix torchbearer tests
    
    * Fix tests

--- tests/callbacks/test_terminate_on_nan.py
+++ tests/callbacks/test_terminate_on_nan.py
@@ -3,94 +3,94 @@ from unittest import TestCase
 from unittest.mock import patch
 
 from torchbearer.callbacks import TerminateOnNaN
-
+import torchbearer
 
 class TestTerminateOnNaN(TestCase):
 
     @patch('sys.stdout', new_callable=StringIO)
     def test_should_terminate(self, _):
         state = {
-            'epoch': 0,
-            't': 0,
-            'stop_training': False,
-            'metrics': {'running_loss': float('nan')}
+            torchbearer.EPOCH: 0,
+            torchbearer.BATCH: 0,
+            torchbearer.STOP_TRAINING: False,
+            torchbearer.METRICS: {'running_loss': float('nan')}
         }
 
         terminator = TerminateOnNaN()
 
         terminator.on_step_training(state)
-        self.assertTrue(state['stop_training'])
+        self.assertTrue(state[torchbearer.STOP_TRAINING])
         terminator.on_step_validation(state)
-        self.assertTrue(state['stop_training'])
+        self.assertTrue(state[torchbearer.STOP_TRAINING])
         terminator.on_end_epoch(state)
-        self.assertTrue(state['stop_training'])
+        self.assertTrue(state[torchbearer.STOP_TRAINING])
 
     def test_should_not_terminate(self):
         state = {
-            'epoch': 0,
-            't': 0,
-            'stop_training': False,
-            'metrics': {'running_loss': 0.01}
+            torchbearer.EPOCH: 0,
+            torchbearer.BATCH: 0,
+            torchbearer.STOP_TRAINING: False,
+            torchbearer.METRICS: {'running_loss': 0.01}
         }
 
         terminator = TerminateOnNaN()
 
         terminator.on_step_training(state)
-        self.assertFalse(state['stop_training'])
+        self.assertFalse(state[torchbearer.STOP_TRAINING])
         terminator.on_step_validation(state)
-        self.assertFalse(state['stop_training'])
+        self.assertFalse(state[torchbearer.STOP_TRAINING])
         terminator.on_end_epoch(state)
-        self.assertFalse(state['stop_training'])
+        self.assertFalse(state[torchbearer.STOP_TRAINING])
 
     @patch('sys.stdout', new_callable=StringIO)
     def test_monitor_should_terminate(self, _):
         state = {
-            'epoch': 0,
-            't': 0,
-            'stop_training': False,
-            'metrics': {'running_loss': 0.001, 'test_metric': float('nan')}
+            torchbearer.EPOCH: 0,
+            torchbearer.BATCH: 0,
+            torchbearer.STOP_TRAINING: False,
+            torchbearer.METRICS: {'running_loss': 0.001, 'test_metric': float('nan')}
         }
 
         terminator = TerminateOnNaN(monitor='test_metric')
 
         terminator.on_step_training(state)
-        self.assertTrue(state['stop_training'])
+        self.assertTrue(state[torchbearer.STOP_TRAINING])
         terminator.on_step_validation(state)
-        self.assertTrue(state['stop_training'])
+        self.assertTrue(state[torchbearer.STOP_TRAINING])
         terminator.on_end_epoch(state)
-        self.assertTrue(state['stop_training'])
+        self.assertTrue(state[torchbearer.STOP_TRAINING])
 
     def test_monitor_should_not_terminate(self):
         state = {
-            'epoch': 0,
-            't': 0,
-            'stop_training': False,
-            'metrics': {'running_loss': 0.01, 'test_metric': 0.001}
+            torchbearer.EPOCH: 0,
+            torchbearer.BATCH: 0,
+            torchbearer.STOP_TRAINING: False,
+            torchbearer.METRICS: {'running_loss': 0.01, 'test_metric': 0.001}
         }
 
         terminator = TerminateOnNaN(monitor='test_metric')
 
         terminator.on_step_training(state)
-        self.assertFalse(state['stop_training'])
+        self.assertFalse(state[torchbearer.STOP_TRAINING])
         terminator.on_step_validation(state)
-        self.assertFalse(state['stop_training'])
+        self.assertFalse(state[torchbearer.STOP_TRAINING])
         terminator.on_end_epoch(state)
-        self.assertFalse(state['stop_training'])
+        self.assertFalse(state[torchbearer.STOP_TRAINING])
 
     @patch('sys.stdout', new_callable=StringIO)
     def test_not_found_metric(self, _):
         state = {
-            'epoch': 0,
-            't': 0,
-            'stop_training': False,
-            'metrics': {'running_loss': 0.001, 'test_metric': float('nan')}
+            torchbearer.EPOCH: 0,
+            torchbearer.BATCH: 0,
+            torchbearer.STOP_TRAINING: False,
+            torchbearer.METRICS: {'running_loss': 0.001, 'test_metric': float('nan')}
         }
 
         terminator = TerminateOnNaN(monitor='test')
 
         terminator.on_step_training(state)
-        self.assertFalse(state['stop_training'])
+        self.assertFalse(state[torchbearer.STOP_TRAINING])
         terminator.on_step_validation(state)
-        self.assertFalse(state['stop_training'])
+        self.assertFalse(state[torchbearer.STOP_TRAINING])
         terminator.on_end_epoch(state)
-        self.assertFalse(state['stop_training'])
+        self.assertFalse(state[torchbearer.STOP_TRAINING])
