ba4e1300549adf249d15ebc2eac4ffe878d25f7c
Ethan Harris <ethanwharris@gmail.com>
2018-08-16 13:54:07 +0100

    Remove unused import

--- tests/callbacks/test_callbacks.py
+++ tests/callbacks/test_callbacks.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from unittest.mock import MagicMock, Mock, PropertyMock
+from unittest.mock import MagicMock, Mock
 
 import torchbearer
 from torchbearer.callbacks import CallbackList, Callback, Tqdm, TensorBoard
