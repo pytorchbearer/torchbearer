aeacfbc8f75c640899a9d08ee97fe24b76d3874d
Ethan Harris <ewah1g13@soton.ac.uk>
2018-08-17 17:26:45 +0100

    Feature/accuracy metrics (#348)
    * Add TopKCategoricalAccuracy metric
    
    * Added binary accuracy metric
    
    * Add MeanSquaredError metric
    
    * Update docs for primitives

--- tests/metrics/test_primitives.py
+++ tests/metrics/test_primitives.py
@@ -1,12 +1,10 @@
 import unittest
-from unittest.mock import patch
 
+import torch
 from torch.autograd import Variable
 
 import torchbearer
-from torchbearer.metrics import Loss, Epoch, CategoricalAccuracy
-
-import torch
+from torchbearer.metrics import Loss, Epoch, CategoricalAccuracy, TopKCategoricalAccuracy, BinaryAccuracy, MeanSquaredError
 
 
 class TestLoss(unittest.TestCase):
@@ -42,6 +40,44 @@ class TestEpoch(unittest.TestCase):
     def test_process_final(self):
         result = self._metric.process_final(self._state)
         self.assertEqual(101, result)
+
+
+class TestBinaryAccuracy(unittest.TestCase):
+    def setUp(self):
+        self._state = {
+            torchbearer.Y_TRUE: torch.LongTensor([
+                [1, 0, 0],
+                [0, 1, 0],
+                [0, 0, 1],
+                [0, 0, 1],
+                [0, 1, 0]
+            ]),
+            torchbearer.Y_PRED: torch.FloatTensor([
+                [0.9, 0.1, 0.1],  # Correct
+                [0.1, 0.9, 0.1],  # Correct
+                [0.1, 0.1, 0.9],  # Correct
+                [0.9, 0.1, 0.1],  # Incorrect
+                [0.9, 0.1, 0.1]  # Incorrect
+            ])
+        }
+        self._targets = [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1]
+        self._metric = BinaryAccuracy().root  # Get root node of Tree for testing
+
+    def test_train_process(self):
+        self._metric.train()
+        result = self._metric.process(self._state)
+        for i in range(0, len(self._targets)):
+            self.assertEqual(result[i], self._targets[i],
+                             msg='returned: ' + str(result[i]) + ' expected: ' + str(self._targets[i])
+                                 + ' in: ' + str(result))
+
+    def test_validate_process(self):
+        self._metric.eval()
+        result = self._metric.process(self._state)
+        for i in range(0, len(self._targets)):
+            self.assertEqual(result[i], self._targets[i],
+                             msg='returned: ' + str(result[i]) + ' expected: ' + str(self._targets[i])
+                                 + ' in: ' + str(result))
 
 
 class TestCategoricalAccuracy(unittest.TestCase):
@@ -83,5 +119,78 @@ class TestCategoricalAccuracy(unittest.TestCase):
         result = self._metric.process(self._state)
         for i in range(0, len(self._targets)):
             self.assertEqual(result[i], self._targets[i],
+                             msg='returned: ' + str(result[i]) + ' expected: ' + str(self._targets[i])
+                                 + ' in: ' + str(result))
+
+
+class TestTopKCategoricalAccuracy(unittest.TestCase):
+    def setUp(self):
+        self._state = {
+            torchbearer.Y_TRUE: Variable(torch.LongTensor([0, 5, 2, 3, 1])),
+            torchbearer.Y_PRED: Variable(torch.FloatTensor([
+                [0.9, 0.8, 0.7, 0.6, 0.5, 0.4],  # Correct
+                [0.4, 0.5, 0.6, 0.7, 0.8, 0.9],  # Correct
+                [0.6, 0.5, 0.4, 0.7, 0.8, 0.9],  # Incorrect
+                [0.6, 0.5, 0.7, 0.4, 0.8, 0.9],  # Incorrect
+                [0.4, 0.5, 0.6, 0.7, 0.8, 0.9]  # Correct
+            ]))
+        }
+        self._targets = [1, 1, 0, 0, 1]
+        self._metric = TopKCategoricalAccuracy(k=5).root  # Get root node of Tree for testing
+
+    def test_ignore_index(self):
+        metric = TopKCategoricalAccuracy(ignore_index=1).root  # Get root node of Tree for testing
+        targets = [1, 1, 0, 0]
+
+        metric.train()
+        result = metric.process(self._state)
+        for i in range(0, len(targets)):
+            self.assertEqual(result[i], targets[i],
+                             msg='returned: ' + str(result[i]) + ' expected: ' + str(targets[i])
+                                 + ' in: ' + str(result))
+
+    def test_train_process(self):
+        self._metric.train()
+        result = self._metric.process(self._state)
+        for i in range(0, len(self._targets)):
+            self.assertEqual(result[i], self._targets[i],
+                             msg='returned: ' + str(result[i]) + ' expected: ' + str(self._targets[i])
+                                 + ' in: ' + str(result))
+
+    def test_validate_process(self):
+        self._metric.eval()
+        result = self._metric.process(self._state)
+        for i in range(0, len(self._targets)):
+            self.assertEqual(result[i], self._targets[i],
+                             msg='returned: ' + str(result[i]) + ' expected: ' + str(self._targets[i])
+                                 + ' in: ' + str(result))
+
+
+class TestMeanSquaredError(unittest.TestCase):
+    def setUp(self):
+        self._state = {
+            torchbearer.Y_TRUE: Variable(torch.FloatTensor(
+                [0.8, 0.2, 0.0, 0.4, 0.3, 0.7]
+            )),
+            torchbearer.Y_PRED: Variable(torch.FloatTensor(
+                [0.9, 0.1, 0.1, 0.7, 0.5, 0.6]
+            ))
+        }
+        self._targets = [0.01, 0.01, 0.01, 0.09, 0.04, 0.01]
+        self._metric = MeanSquaredError().root  # Get root node of Tree for testing
+
+    def test_train_process(self):
+        self._metric.train()
+        result = self._metric.process(self._state)
+        for i in range(0, len(self._targets)):
+            self.assertAlmostEqual(result[i].item(), self._targets[i], places=3,
+                             msg='returned: ' + str(result[i]) + ' expected: ' + str(self._targets[i])
+                                 + ' in: ' + str(result))
+
+    def test_validate_process(self):
+        self._metric.eval()
+        result = self._metric.process(self._state)
+        for i in range(0, len(self._targets)):
+            self.assertAlmostEqual(result[i].item(), self._targets[i], places=3,
                              msg='returned: ' + str(result[i]) + ' expected: ' + str(self._targets[i])
                                  + ' in: ' + str(result))
--- torchbearer/metrics/primitives.py
+++ torchbearer/metrics/primitives.py
@@ -3,7 +3,10 @@ Base metrics are the base classes which represent the metrics supplied with torc
 :func:`.default_for_key` decorator so that they can be accessed in the call to :class:`.torchbearer.Model` via the
 following strings:
 
+- '`binary_acc`': The :class:`.BinaryAccuracy` metric
 - '`acc`' or '`accuracy`': The :class:`.CategoricalAccuracy` metric
+- '`top_5_acc`': The :class:`.TopKCategoricalAccuracy` metric
+- '`mse`': The :class:`.MeanSquaredError` metric
 - '`loss`': The :class:`.Loss` metric
 - '`epoch`': The :class:`.Epoch` metric
 - '`roc_auc`' or '`roc_auc_score`': The :class:`.RocAucScore` metric
@@ -12,6 +15,25 @@ import torchbearer
 from torchbearer import metrics
 
 import torch
+
+
+@metrics.default_for_key('binary_acc')
+@metrics.running_mean
+@metrics.mean
+class BinaryAccuracy(metrics.Metric):
+    """Binary accuracy metric. Uses torch.eq to compare predictions to targets. Decorated with a mean and running_mean.
+    Default for key: 'binary_acc'.
+    """
+
+    def __init__(self):
+        super().__init__('binary_acc')
+
+    def process(self, *args):
+        state = args[0]
+        y_pred = state[torchbearer.Y_PRED]
+        y_true = state[torchbearer.Y_TRUE]
+
+        return torch.eq(torch.round(y_pred).type(y_true.type()), y_true).view(-1).float()
 
 
 @metrics.default_for_key('acc')
@@ -41,6 +63,54 @@ class CategoricalAccuracy(metrics.Metric):
         y_true = y_true[mask]
         _, y_pred = torch.max(y_pred, 1)
         return (y_pred == y_true).float()
+
+
+@metrics.default_for_key('top_5_acc')
+@metrics.running_mean
+@metrics.std
+@metrics.mean
+class TopKCategoricalAccuracy(metrics.Metric):
+    """Top K Categorical accuracy metric. Uses torch.topk to determine the top k predictions and compares to targets.
+    Decorated with a mean, running_mean and std. Default for key: 'top_5_acc'.
+
+    :param ignore_index: Specifies a target value that is ignored and does not contribute to the metric output. See `<https://pytorch.org/docs/stable/nn.html#crossentropyloss>`_
+    :type ignore_index: int
+    """
+
+    def __init__(self, k=5, ignore_index=-100):
+        super().__init__('top_' + str(k) + '_acc')
+        self.k = k
+        self.ignore_index = ignore_index
+
+    def process(self, *args):
+        state = args[0]
+        y_pred = state[torchbearer.Y_PRED]
+        y_true = state[torchbearer.Y_TRUE]
+        mask = y_true.eq(self.ignore_index).eq(0)
+        y_pred = y_pred[mask]
+        y_true = y_true[mask]
+
+        sorted_indices = torch.topk(y_pred, self.k, dim=1)[1]
+        expanded_y = y_true.view(-1, 1).expand(-1, self.k)
+        return torch.sum(torch.eq(sorted_indices, expanded_y), dim=1).float()
+
+
+@metrics.default_for_key('mse')
+@metrics.running_mean
+@metrics.mean
+class MeanSquaredError(metrics.Metric):
+    """Mean squared error metric. Computes the pixelwise squared error which is then averaged with decorators.
+    Decorated with a mean and running_mean. Default for key: 'mse'.
+    """
+
+    def __init__(self):
+        super().__init__('mse')
+
+    def process(self, *args):
+        state = args[0]
+        y_pred = state[torchbearer.Y_PRED]
+        y_true = state[torchbearer.Y_TRUE]
+        return torch.pow(y_pred - y_true.view_as(y_pred), 2)
 
 
 @metrics.default_for_key('loss')
