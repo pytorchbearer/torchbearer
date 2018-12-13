3bb236d7fa9e4253911792af56a18913b4fe7722
Matt <mp2u16@soton.ac.uk>
2018-12-13 18:42:04 +0000

    Add docs

---
+++ tests/callbacks/test_live_loss_plot.py
@@ -0,0 +1,68 @@
+from unittest import TestCase
+from unittest.mock import patch, Mock, MagicMock
+
+import torchbearer as tb
+from torchbearer.callbacks import LiveLossPlot
+
+
+class TestLiveLossPlot(TestCase):
+
+    def test_on_batch(self):
+        llp = LiveLossPlot(True, 1, False, False)
+        llp.batch_plt = MagicMock()
+        llp.plt = MagicMock()
+        state = {tb.BATCH: 1, tb.METRICS: {'test': 1}}
+        llp.on_step_training(state)
+        llp.on_step_training(state)
+
+        self.assertTrue(llp.batch_plt.update.call_count == 2)
+        self.assertTrue(llp.plt.update.call_count == 0)
+
+    def test_on_batch_steps(self):
+        llp = LiveLossPlot(True, 2, False, False)
+        llp.batch_plt = MagicMock()
+        llp.plt = MagicMock()
+        state = {tb.BATCH: 1, tb.METRICS: {'test': 1}}
+        llp.on_step_training(state)
+        state = {tb.BATCH: 2, tb.METRICS: {'test': 1}}
+        llp.on_step_training(state)
+        state = {tb.BATCH: 3, tb.METRICS: {'test': 1}}
+        llp.on_step_training(state)
+        state = {tb.BATCH: 4, tb.METRICS: {'test': 1}}
+        llp.on_step_training(state)
+
+        self.assertTrue(llp.batch_plt.draw.call_count == 2)
+        self.assertTrue(llp.plt.draw.call_count == 0)
+
+    def test_not_on_batch(self):
+        llp = LiveLossPlot(False, 10, False, False)
+        llp.batch_plt = MagicMock()
+        llp.plt = MagicMock()
+        state = {tb.BATCH: 1, tb.METRICS: {'test': 1}}
+        llp.on_step_training(state)
+        llp.on_step_training(state)
+
+        self.assertTrue(llp.batch_plt.update.call_count == 0)
+
+    def test_on_epoch(self):
+        llp = LiveLossPlot(False, 10, True, False)
+        llp.batch_plt = MagicMock()
+        llp.plt = MagicMock()
+        state = {tb.BATCH: 1, tb.METRICS: {'test': 1}}
+        llp.on_end_epoch(state)
+        llp.on_end_epoch(state)
+
+        self.assertTrue(llp.batch_plt.update.call_count == 0)
+        self.assertTrue(llp.plt.update.call_count == 2)
+
+    def test_draw_once(self):
+        llp = LiveLossPlot(True, 1, True, True)
+        llp.batch_plt = MagicMock()
+        llp.plt = MagicMock()
+        state = {tb.BATCH: 1, tb.METRICS: {'test': 1}}
+        llp.on_end_epoch(state)
+        llp.on_end_epoch(state)
+        llp.on_end(state)
+
+        self.assertTrue(llp.plt.draw.call_count == 1)
+        self.assertTrue(llp.batch_plt.draw.call_count == 1)
--- torchbearer/callbacks/live_loss_plot.py
+++ torchbearer/callbacks/live_loss_plot.py
@@ -1,9 +1,37 @@
-import torchbearer as tb
-import torchbearer.callbacks as c
+import sys
+from contextlib import ContextDecorator
 
+import torchbearer
+from torchbearer.callbacks import Callback
 
-class LiveLossPlot(c.Callback):
+class no_print(ContextDecorator):
+    def __init__(self):
+        super().__init__()
+        self.null_obj = lambda: None
+        self.null_obj.write = lambda x: ...
+        self.null_obj.flush = lambda: ...
+
+    def __enter__(self):
+        self.stdout = sys.stdout
+        sys.stdout = self.null_obj
+        return self
+
+    def __exit__(self, *exc):
+        sys.stdout = self.stdout
+        return False
+
+class LiveLossPlot(Callback):
+    """
+    Callback to write metrics to `LiveLossPlot <https://github.com/stared/livelossplot>`_, a library for visualisation in notebooks
+
+    :param on_batch: If True, batch metrics will be logged. Else batch metrics will not be logged
+    :param batch_step_size: The number of batches between logging metrics
+    :param on_epoch: If True, epoch metrics will be logged every epoch. Else epoch metrics will not be logged
+    :param draw_once: If True, draw the plot only at the end of training. Else draw every time metrics are logged
+    :param kwargs: Keyword arguments for livelossplot.PlotLosses
+    """
     def __init__(self, on_batch=False, batch_step_size=10, on_epoch=True, draw_once=False, **kwargs):
+
         super().__init__()
         from livelossplot import PlotLosses
         self.plt = PlotLosses(**kwargs)
@@ -20,16 +48,21 @@ class LiveLossPlot(c.Callback):
         if on_epoch:
             self.on_end_epoch = self._on_end_epoch
 
+    @no_print()
+    def draw(self, plt):
+        plt.draw()
+
     def _on_step_training(self, state):
-        self.batch_plt.update(state[tb.METRICS])
-        if state[tb.BATCH] % self.batch_step_size == 0 and not self.draw_once:
-            self.batch_plt.draw()
+        self.batch_plt.update(state[torchbearer.METRICS])
+        if state[torchbearer.BATCH] % self.batch_step_size == 0 and not self.draw_once:
+            self.draw(self.batch_plt)
 
     def _on_end_epoch(self, state):
-        self.plt.update(state[tb.METRICS])
+        self.plt.update(state[torchbearer.METRICS])
         if not self.draw_once:
-            self.plt.draw()
+            self.draw(self.plt)
 
     def on_end(self, state):
         if self.draw_once:
-            self.plt.draw()
+            self.draw(self.batch_plt)
+            self.draw(self.plt)
