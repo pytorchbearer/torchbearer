acbbf36abd635e32bec69f8ead5726af4301fea5
Matt <mp2u16@soton.ac.uk>
2019-01-15 17:31:34 +0000

    Add simple uniform and exponential distributions

--- torchbearer/variational/distributions.py
+++ torchbearer/variational/distributions.py
@@ -37,3 +37,32 @@ class SimpleGaussian(DistributionBase):
         var = self.logvar.exp()
         return (value - self.mu)**2/(2*var) - ((2*np.pi*var)**0.5).log()
 
+
+class SimpleUniform(DistributionBase):
+    def __init__(self, low, high):
+        super().__init__()
+        self.low = low
+        self.high = high
+
+    def rsample(self, sample_shape=torch.Size()):
+        rand = torch.rand(sample_shape, dtype=self.low.dtype, device=self.low.device)
+        return self.low + rand * (self.high - self.low)
+
+    def log_prob(self, value):
+        lb = value.ge(self.low).type_as(self.low)
+        ub = value.lt(self.high).type_as(self.low)
+        return torch.log(lb.mul(ub)) - torch.log(self.high - self.low)
+
+
+class SimpleExponential(DistributionBase):
+    def __init__(self, rate):
+        super().__init__()
+        self.rate = rate
+
+    def rsample(self, sample_shape=torch.Size()):
+        shape = sample_shape
+        return self.rate.new(shape).exponential_() / self.rate
+
+    def log_prob(self, value):
+        return self.rate.log() - self.rate * value
+
