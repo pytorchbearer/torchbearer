8ae7791ef6fd8556eb9ccafa400c406ae57052f2
Ethan Harris <ethanwharris@gmail.com>
2019-01-17 10:40:16 +0000

    Add tests for SimpleUniform distribution

--- tests/variational/test_distributions.py
+++ tests/variational/test_distributions.py
@@ -4,7 +4,7 @@ from unittest.mock import patch
 
 import torch
 
-from torchbearer.variational import SimpleNormal
+from torchbearer.variational import SimpleNormal, SimpleUniform
 
 
 class TestSimpleNormal(unittest.TestCase):
@@ -23,15 +23,13 @@ class TestSimpleNormal(unittest.TestCase):
 
     @patch('torchbearer.variational.distributions.torch.normal')
     def test_rsample_number(self, normal):
-        mu = torch.ones(2, 2)
-
-        dist = SimpleNormal(mu, 0)
+        dist = SimpleNormal(1, 0)
 
         normal.side_effect = lambda mu, std: mu
-        self.assertTrue(dist.rsample(sample_shape=torch.Size([2])).sum() == 8)
+        self.assertTrue(dist.rsample(sample_shape=torch.Size([2])).sum() == 2)
 
         normal.side_effect = lambda mu, std: std
-        self.assertTrue(dist.rsample(sample_shape=torch.Size([2])).sum() == 16)
+        self.assertTrue(dist.rsample(sample_shape=torch.Size([2])).sum() == 4)
 
     def test_logprob_tensor(self):
         mu = torch.ones(2, 2)
@@ -39,12 +37,45 @@ class TestSimpleNormal(unittest.TestCase):
 
         dist = SimpleNormal(mu, logvar)
 
-        self.assertTrue(((dist.log_prob(torch.ones(2, 2)) + 1.2655).abs() < 0.0001).sum() == 4)
+        self.assertTrue(((dist.log_prob(torch.ones(2, 2)) + 1.2655).abs() < 0.0001).all())
 
     def test_logprob_number(self):
-        mu = torch.ones(2, 2)
         logvar = math.log(2)
 
-        dist = SimpleNormal(mu, logvar)
+        dist = SimpleNormal(1, logvar)
 
-        self.assertTrue(((dist.log_prob(torch.ones(2, 2)) + 1.2655).abs() < 0.0001).sum() == 4)
+        self.assertTrue(((dist.log_prob(1) + 1.2655).abs() < 0.0001).all())
+
+
+class TestSimpleUniform(unittest.TestCase):
+    @patch('torchbearer.variational.distributions.torch.rand')
+    def test_rsample_tensor(self, rand):
+        low = torch.ones(2, 2)
+        high = torch.ones(2, 2) + 1
+
+        dist = SimpleUniform(low, high)
+
+        rand.side_effect = lambda shape, dtype, device: torch.ones(shape) / 2
+        self.assertTrue(((dist.rsample(sample_shape=torch.Size([2])) - 1.5).abs() < 0.0001).all())
+
+    @patch('torchbearer.variational.distributions.torch.rand')
+    def test_rsample_number(self, rand):
+        dist = SimpleUniform(1, 2)
+
+        rand.side_effect = lambda shape, dtype, device: torch.ones(shape) / 2
+        self.assertTrue(((dist.rsample(sample_shape=torch.Size([2])) - 1.5).abs() < 0.0001).all())
+
+    def test_logprob_tensor(self):
+        low = torch.ones(2, 2)
+        high = torch.ones(2, 2) + 1
+
+        dist = SimpleUniform(low, high)
+
+        self.assertTrue((dist.log_prob(torch.ones(2, 2)).abs() < 0.0001).all())
+        self.assertTrue((dist.log_prob(torch.ones(2, 2) + 2) == float('-inf')).all())
+
+    def test_logprob_number(self):
+        dist = SimpleUniform(1, 2)
+
+        self.assertTrue((dist.log_prob(1).abs() < 0.0001).all())
+        self.assertTrue((dist.log_prob(3) == float('-inf')).all())
--- torchbearer/variational/distributions.py
+++ torchbearer/variational/distributions.py
@@ -3,6 +3,7 @@ from numbers import Number
 
 import torch
 from torch.distributions import Distribution
+from torch.distributions.utils import broadcast_all
 
 
 class SimpleDistribution(Distribution):
@@ -61,38 +62,94 @@ class SimpleDistribution(Distribution):
 
 
 class SimpleNormal(SimpleDistribution):
+    """The SimpleNormal class is a :class:`SimpleDistribution` which implements a straight forward Normal / Gaussian
+    distribution. This performs significantly fewer checks than `torch.distributions.Normal`, but should be sufficient
+    for the purpose of implementing a VAE.
+
+    Args:
+        mu (torch.Tensor, Number): The mean of the distribution, numbers will be cast to tensors
+        logvar (torch.Tensor, Number): The log variance of the distribution, numbers will be cast to tensors
+    """
     def __init__(self, mu, logvar):
-        self.mu = mu
-        self.logvar = logvar
+        self.mu, self.logvar = broadcast_all(mu, logvar)
         if isinstance(mu, Number) and isinstance(logvar, Number):
             batch_shape = torch.Size()
         else:
-            batch_shape = mu.size()
+            batch_shape = self.mu.size()
         super().__init__(batch_shape=batch_shape)
 
     def rsample(self, sample_shape=torch.Size()):
+        """Simple rsample for a Normal distribution.
+
+        Args:
+            sample_shape: Shape of the sample (per mean / variance given)
+
+        Returns:
+            A reparameterized sample with gradient with respect to the distribution parameters
+        """
         shape = self._extended_shape(sample_shape)
-        std = math.exp(self.logvar / 2.0) if isinstance(self.logvar, Number) else self.logvar.div(2).exp_()
+        std = self.logvar.div(2).exp_()
         eps = torch.normal(torch.zeros(shape, dtype=self.mu.dtype, device=self.mu.device),
                            torch.ones(shape, dtype=self.mu.dtype, device=self.mu.device))
         return self.mu + std * eps
 
     def log_prob(self, value):
-        var = math.exp(self.logvar) if isinstance(self.logvar, Number) else self.logvar.exp()
+        """Calculates the log probability that the given value was drawn from this distribution. Since the density of a
+        Gaussian is differentiable, this function is differentiable.
+
+        Args:
+            value (torch.Tensor, Number): The sampled value
+
+        Returns:
+            The log probability that the given value was drawn from this distribution
+        """
+        var = self.logvar.exp()
         return - ((value - self.mu) ** 2) / (2.0 * var) - (self.logvar / 2.0) - math.log(math.sqrt(2.0 * math.pi))
 
 
 class SimpleUniform(SimpleDistribution):
+    """The SimpleUniform class is a :class:`SimpleDistribution` which implements a straight forward Uniform distribution
+    in the interval ``[low, high)``. This performs significantly fewer checks than `torch.distributions.Uniform`, but
+    should be sufficient for the purpose of implementing a VAE.
+
+    Args:
+        low (torch.Tensor, Number): The lower range of the distribution (inclusive), numbers will be cast to tensors
+        high (torch.Tensor, Number): The upper range of the distribution (exclusive), numbers will be cast to tensors
+    """
     def __init__(self, low, high):
         super().__init__()
-        self.low = low
-        self.high = high
+        self.low, self.high = broadcast_all(low, high)
+        if isinstance(low, Number) and isinstance(high, Number):
+            batch_shape = torch.Size()
+        else:
+            batch_shape = self.low.size()
+        super().__init__(batch_shape=batch_shape)
 
     def rsample(self, sample_shape=torch.Size()):
-        rand = torch.rand(sample_shape, dtype=self.low.dtype, device=self.low.device)
+        """Simple rsample for a Uniform distribution.
+
+        Args:
+            sample_shape: Shape of the sample (per low / high given)
+
+        Returns:
+            A reparameterized sample with gradient with respect to the distribution parameters
+        """
+        shape = self._extended_shape(sample_shape)
+        rand = torch.rand(shape, dtype=self.low.dtype, device=self.low.device)
         return self.low + rand * (self.high - self.low)
 
     def log_prob(self, value):
+        """Calculates the log probability that the given value was drawn from this distribution. Since this distribution
+        is uniform, the log probability is zero for all values in the range ``[low, high)`` and -inf elsewhere. This
+        function is therefore non-differentiable.
+
+        Args:
+            value (torch.Tensor, Number): The sampled value
+
+        Returns:
+            The log probability that the given value was drawn from this distribution
+        """
+        value = value if torch.is_tensor(value) else torch.tensor(value, dtype=torch.get_default_dtype())
         lb = value.ge(self.low).type_as(self.low)
         ub = value.lt(self.high).type_as(self.low)
         return torch.log(lb.mul(ub)) - torch.log(self.high - self.low)
