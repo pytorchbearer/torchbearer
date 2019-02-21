c2a9e74037a755d1bb1eafa76347a9af21318a35
Matt <mp2u16@soton.ac.uk>
2019-02-21 12:01:31 +0000

    Add test

--- tests/callbacks/test_aggregate_predictions.py
+++ tests/callbacks/test_aggregate_predictions.py
@@ -26,4 +26,22 @@ class TestAggregatePredictions(TestCase):
         aggregator.on_end_validation(final_state)
         self.assertTrue(list(final_state[tb.FINAL_PREDICTIONS].numpy()) == list(aggregate.numpy()))
 
+    def test_aggreate_predictions_multiple_calls(self):
+        aggregator = AggregatePredictions()
+
+        y_pred_1 = torch.Tensor([1,2,3])
+        y_pred_2 = torch.Tensor([3,4,5])
+
+        state_1 = {tb.Y_PRED: y_pred_1}
+        state_2 = {tb.Y_PRED: y_pred_2}
+
+        aggregator.on_step_validation(state_1)
+        self.assertTrue(list(aggregator.predictions_list[0].numpy()) == list(y_pred_1.numpy()))
+
+        aggregator.on_step_validation(state_2)
+        self.assertTrue(list(aggregator.predictions_list[1].numpy()) == list(y_pred_2.numpy()))
+
+        aggregator.on_end_epoch(state_2)
+        self.assertTrue(list(aggregator.predictions_list) == [])
+
 
