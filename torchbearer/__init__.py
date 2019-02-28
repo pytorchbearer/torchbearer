b19e001f4d0c2b2782d52ce5953b6a567229283e
Matt Painter <mp2u16@ecs.soton.ac.uk>
2019-02-28 14:19:33 +0000

    Add torchbearer notebook line magic (#517)
    * Add torchbearer notebook line magic
    
    * Move magic
    
    * Add test
    
    * Update changelog
    
    * Fix test in py2
    
    * Fix test in py2
    
    * Fix test
    
    * Formatting
    
    * Add tqdm test

--- torchbearer/__init__.py
+++ torchbearer/__init__.py
@@ -29,6 +29,7 @@ Utilities
 """
 
 from .version import __version__
+from . import magics
 from .cite import cite
 from .bases import *
 from .state import *
---
+++ torchbearer/magics.py
@@ -0,0 +1,25 @@
+# Sets global variable notebook when line magic is called with "%torchbearer notebook"global notebook
+notebook = {'nb': False}
+
+
+def set_notebook(is_notebook):
+    notebook['nb'] = is_notebook
+
+
+def torchbearer(line):
+    if line == 'notebook':
+        set_notebook(True)
+    elif line == 'normal':
+        set_notebook(False)
+
+
+try:
+    import IPython.core.magic
+    torchbearer = IPython.core.magic.register_line_magic(torchbearer)
+    set_notebook(True)
+except NameError:
+    pass
+
+
+def is_notebook():
+    return notebook['nb']
