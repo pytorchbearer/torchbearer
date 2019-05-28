0fc0feba0778b3a5774ce54e1f1143cb2a7fb8d6
Ethan Harris <ethanwharris@gmail.com>
2019-05-28 14:05:26 +0100

    Fix import error

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ else:
 setup(
     name='torchbearer',
     version=version_dict['__version__'],
-    packages=['torchbearer', 'torchbearer.metrics', 'torchbearer.callbacks', 'torchbearer.variational', 'tests', 'tests.metrics', 'tests.callbacks', 'tests.variational'],
+    packages=['torchbearer', 'torchbearer.metrics', 'torchbearer.callbacks', 'torchbearer.callbacks.imaging', 'torchbearer.variational', 'tests', 'tests.metrics', 'tests.callbacks', 'tests.callbacks.imaging', 'tests.variational'],
     url='https://github.com/ecs-vlc/torchbearer',
     download_url='https://github.com/ecs-vlc/torchbearer/archive/' + version_dict['__version__'] + '.tar.gz',
     license='GPL-3.0',
