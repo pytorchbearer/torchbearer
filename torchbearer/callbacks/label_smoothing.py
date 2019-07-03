5ca415c9a294bdb3c1c990704b073356874ca6d5
Matt <mp2u16@soton.ac.uk>
2019-07-03 13:45:25 +0100

    Fix label smoothing on val

--- torchbearer/callbacks/label_smoothing.py
+++ torchbearer/callbacks/label_smoothing.py
@@ -21,6 +21,19 @@ class LabelSmoothingRegularisation(Callback):
     """Perform Label Smoothing Regularisation (LSR) on the targets during training. This involves converting the target
     to a one-hot vector and smoothing according to the value epsilon.
 
+    .. note::
+
+        Requires a multi-label loss, such as nn.BCELoss
+
+    Example: ::
+
+        >>> from torchbearer import Trial
+        >>> from torchbearer.callbacks import LabelSmoothingRegularisation
+
+        # Example Trial which does label smoothing regularisation
+        >>> smoothing = LabelSmoothingRegularisation()
+        >>> trial = Trial(None, criterion=nn.BCELoss(), callbacks=[smoothing], metrics=['acc'])
+
     Args:
         epsilon (float): The epsilon parameter from the paper
         classes (int): The number of target classes, not required if the target is already one-hot encoded
@@ -29,7 +42,7 @@ class LabelSmoothingRegularisation(Callback):
         self.epsilon = epsilon
         self.classes = classes
 
-    def on_sample(self, state):
+    def to_one_hot(self, state):
         target = state[torchbearer.TARGET]
 
         if target.dim() == 1:
@@ -37,6 +50,13 @@ class LabelSmoothingRegularisation(Callback):
             one_hot = torch.zeros_like(target).repeat(1, self.classes)
             one_hot.scatter_(1, target, 1)
             target = one_hot
+        return target
 
+    def on_sample(self, state):
+        target = self.to_one_hot(state)
         target = (1 - self.epsilon) * target.float() + (self.epsilon / target.size(1))
         state[torchbearer.TARGET] = target
+
+    def on_sample_validation(self, state):
+        target = self.to_one_hot(state)
+        state[torchbearer.TARGET] = target.float()
