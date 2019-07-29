484cc13f3035b1fea33e1e8703d9a3f422caa61f
Matt <mp2u16@soton.ac.uk>
2019-07-29 10:11:53 +0100

    Fix final predictions empty

--- torchbearer/callbacks/aggregate_predictions.py
+++ torchbearer/callbacks/aggregate_predictions.py
@@ -13,9 +13,11 @@ class AggregatePredictions(Callback):
         self.predictions_list.append(state[torchbearer.Y_PRED])
 
     def on_end_validation(self, state):
-
-        state[torchbearer.FINAL_PREDICTIONS] = torch.cat(self.predictions_list, 0) if len(self.predictions_list) != 1 \
-            else self.predictions_list[0]
+        if len(self.predictions_list) > 0:
+            state[torchbearer.FINAL_PREDICTIONS] = torch.cat(self.predictions_list, 0) if len(self.predictions_list) \
+                                                                                    != 1 else self.predictions_list[0]
+        else:
+            state[torchbearer.FINAL_PREDICTIONS] = []
 
     def on_end_epoch(self, state):
         super(AggregatePredictions, self).on_end_epoch(state)
