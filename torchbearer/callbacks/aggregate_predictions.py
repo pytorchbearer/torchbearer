b7023e019fb29a3ad89a163e13e36fb5f2203cbb
Matt <mp2u16@soton.ac.uk>
2019-07-29 10:54:01 +0100

    Fix finding none in list

--- torchbearer/callbacks/aggregate_predictions.py
+++ torchbearer/callbacks/aggregate_predictions.py
@@ -13,7 +13,8 @@ class AggregatePredictions(Callback):
         self.predictions_list.append(state[torchbearer.Y_PRED])
 
     def on_end_validation(self, state):
-        if len(self.predictions_list) > 0 and not None in self.predictions_list:
+        # Hack to check if None is in a list alongside tensors 
+        if len(self.predictions_list) > 0 and not 1 in [i is None for i in self.predictions_list]:
             state[torchbearer.FINAL_PREDICTIONS] = torch.cat(self.predictions_list, 0) if len(self.predictions_list) \
                                                                                     != 1 else self.predictions_list[0]
         else:
