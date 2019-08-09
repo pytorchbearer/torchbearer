a5b77603af20fd20c5d8a19f702f9856f1344ca8
Matt <mp2u16@soton.ac.uk>
2019-08-09 15:34:49 +0100

    Print traceback properly

--- torchbearer/bases.py
+++ torchbearer/bases.py
@@ -1,5 +1,6 @@
 from distutils.version import LooseVersion
 import functools
+import traceback
 
 import torch
 import torchbearer
@@ -311,6 +312,7 @@ def _forward_with_exceptions(x, model, y_pred, state):
 
     # Forward Pass
     try:
+        exc_info = sys.exc_info()
         state[y_pred] = state[model](dx, state=state)
     except Exception as e:
         error = []
@@ -326,6 +328,8 @@ def _forward_with_exceptions(x, model, y_pred, state):
                 error.append(e)
             error.append(e2)
             raise Exception(error)
+    finally:
+        traceback.print_exception(*exc_info)
 
 
 def base_closure(x, model, y_pred, y_true, crit, loss, opt):
