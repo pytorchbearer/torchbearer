8725cffcb374c42cb7e25ef529284d7d995fefd5
Ethan Harris <ewah1g13@soton.ac.uk>
2021-02-28 13:44:05 +0000

    Remove last_epoch argument from scheduler callbacks, clean up tests

--- tests/callbacks/test_torch_scheduler.py
+++ tests/callbacks/test_torch_scheduler.py
@@ -21,7 +21,7 @@ class TestTorchScheduler(TestCase):
         mock_scheduler = Mock()
         mock_scheduler.return_value = mock_scheduler
 
-        torch_scheduler = TorchScheduler(lambda opt: mock_scheduler(opt), monitor='test', step_on_batch=True)
+        torch_scheduler = TorchScheduler(mock_scheduler, monitor='test', step_on_batch=True)
         torch_scheduler._newstyle = True
 
         import warnings
@@ -53,7 +53,7 @@ class TestTorchScheduler(TestCase):
         mock_scheduler = Mock()
         mock_scheduler.return_value = mock_scheduler
 
-        torch_scheduler = TorchScheduler(lambda opt: mock_scheduler(opt), monitor='test', step_on_batch=True)
+        torch_scheduler = TorchScheduler(mock_scheduler, monitor='test', step_on_batch=True)
         torch_scheduler._newstyle = False
 
         import warnings
@@ -86,7 +86,7 @@ class TestTorchScheduler(TestCase):
         mock_scheduler = Mock()
         mock_scheduler.return_value = mock_scheduler
 
-        torch_scheduler = TorchScheduler(lambda opt: mock_scheduler(opt), monitor='test', step_on_batch=False)
+        torch_scheduler = TorchScheduler(mock_scheduler, monitor='test', step_on_batch=False)
         torch_scheduler._newstyle = True
 
         torch_scheduler.on_start(state)
@@ -115,7 +115,7 @@ class TestTorchScheduler(TestCase):
         mock_scheduler = Mock()
         mock_scheduler.return_value = mock_scheduler
 
-        torch_scheduler = TorchScheduler(lambda opt: mock_scheduler(opt), monitor='test', step_on_batch=False)
+        torch_scheduler = TorchScheduler(mock_scheduler, monitor='test', step_on_batch=False)
         torch_scheduler._newstyle = False
 
         torch_scheduler.on_start(state)
@@ -143,7 +143,7 @@ class TestTorchScheduler(TestCase):
         mock_scheduler = Mock()
         mock_scheduler.return_value = mock_scheduler
 
-        torch_scheduler = TorchScheduler(lambda opt: mock_scheduler(opt), monitor=None, step_on_batch=True)
+        torch_scheduler = TorchScheduler(mock_scheduler, monitor=None, step_on_batch=True)
         torch_scheduler._newstyle = True
 
         torch_scheduler.on_start(state)
@@ -171,7 +171,7 @@ class TestTorchScheduler(TestCase):
         mock_scheduler = Mock()
         mock_scheduler.return_value = mock_scheduler
 
-        torch_scheduler = TorchScheduler(lambda opt: mock_scheduler(opt), monitor=None, step_on_batch=True)
+        torch_scheduler = TorchScheduler(mock_scheduler, monitor=None, step_on_batch=True)
         torch_scheduler._newstyle = False
 
         torch_scheduler.on_start(state)
@@ -199,7 +199,7 @@ class TestTorchScheduler(TestCase):
         mock_scheduler = Mock()
         mock_scheduler.return_value = mock_scheduler
 
-        torch_scheduler = TorchScheduler(lambda opt: mock_scheduler(opt), monitor=None, step_on_batch=False)
+        torch_scheduler = TorchScheduler(mock_scheduler, monitor=None, step_on_batch=False)
         torch_scheduler._newstyle = True
 
         torch_scheduler.on_start(state)
@@ -223,7 +223,7 @@ class TestTorchScheduler(TestCase):
         mock_scheduler = Mock()
         mock_scheduler.return_value = mock_scheduler
 
-        torch_scheduler = TorchScheduler(lambda opt: mock_scheduler(opt), monitor=None, step_on_batch=False)
+        torch_scheduler = TorchScheduler(mock_scheduler, monitor=None, step_on_batch=False)
         torch_scheduler._newstyle = False
 
         torch_scheduler.on_start(state)
@@ -251,7 +251,7 @@ class TestTorchScheduler(TestCase):
         mock_scheduler = Mock()
         mock_scheduler.return_value = mock_scheduler
 
-        torch_scheduler = TorchScheduler(lambda opt: mock_scheduler(opt), monitor='test', step_on_batch=False)
+        torch_scheduler = TorchScheduler(mock_scheduler, monitor='test', step_on_batch=False)
         torch_scheduler.on_start(state)
 
         with warnings.catch_warnings(record=True) as w:
@@ -267,7 +267,7 @@ class TestTorchScheduler(TestCase):
         mock_scheduler = Mock()
         mock_scheduler.return_value = mock_scheduler
 
-        torch_scheduler = TorchScheduler(lambda opt: mock_scheduler(opt), monitor='test', step_on_batch=False)
+        torch_scheduler = TorchScheduler(mock_scheduler, monitor='test', step_on_batch=False)
         torch_scheduler.on_start(state)
         with warnings.catch_warnings(record=True) as w:
             torch_scheduler.on_start_training(state)
@@ -286,7 +286,7 @@ class TestTorchScheduler(TestCase):
         mock_scheduler = Mock()
         mock_scheduler.return_value = mock_scheduler
 
-        torch_scheduler = TorchScheduler(lambda opt: mock_scheduler(opt), monitor='test', step_on_batch=True)
+        torch_scheduler = TorchScheduler(mock_scheduler, monitor='test', step_on_batch=True)
         torch_scheduler.on_start(state)
 
         with warnings.catch_warnings(record=True) as w:
@@ -298,7 +298,7 @@ class TestTorchScheduler(TestCase):
         mock_scheduler = Mock()
         mock_scheduler.return_value = mock_scheduler
 
-        torch_scheduler = TorchScheduler(lambda opt: mock_scheduler(opt), monitor='test', step_on_batch=True)
+        torch_scheduler = TorchScheduler(mock_scheduler, monitor='test', step_on_batch=True)
         torch_scheduler.on_start(state)
 
         with warnings.catch_warnings(record=True) as w:
@@ -309,74 +309,74 @@ class TestTorchScheduler(TestCase):
 class TestLambdaLR(TestCase):
     @patch('torch.optim.lr_scheduler.LambdaLR')
     def test_lambda_lr(self, lr_mock):
-        state = {torchbearer.OPTIMIZER: 'optimizer'}
+        state = {torchbearer.OPTIMIZER: 'optimizer', torchbearer.EPOCH: 0}
 
-        scheduler = LambdaLR(0.1, last_epoch=-4, step_on_batch=True)
+        scheduler = LambdaLR(lr_lambda=0.1, step_on_batch=True)
         scheduler.on_start(state)
 
-        lr_mock.assert_called_once_with('optimizer', 0.1, last_epoch=-4)
+        lr_mock.assert_called_once_with('optimizer', lr_lambda=0.1, last_epoch=-1)
         self.assertTrue(scheduler._step_on_batch)
 
 
 class TestStepLR(TestCase):
     @patch('torch.optim.lr_scheduler.StepLR')
     def test_lambda_lr(self, lr_mock):
-        state = {torchbearer.OPTIMIZER: 'optimizer'}
+        state = {torchbearer.OPTIMIZER: 'optimizer', torchbearer.EPOCH: 0}
 
-        scheduler = StepLR(10, gamma=0.4, last_epoch=-4, step_on_batch=True)
+        scheduler = StepLR(step_size=10, gamma=0.4, step_on_batch=True)
         scheduler.on_start(state)
 
-        lr_mock.assert_called_once_with('optimizer', 10, gamma=0.4, last_epoch=-4)
+        lr_mock.assert_called_once_with('optimizer', step_size=10, gamma=0.4, last_epoch=-1)
         self.assertTrue(scheduler._step_on_batch)
 
 
 class TestMultiStepLR(TestCase):
     @patch('torch.optim.lr_scheduler.MultiStepLR')
     def test_lambda_lr(self, lr_mock):
-        state = {torchbearer.OPTIMIZER: 'optimizer'}
+        state = {torchbearer.OPTIMIZER: 'optimizer', torchbearer.EPOCH: 0}
 
-        scheduler = MultiStepLR(10, gamma=0.4, last_epoch=-4, step_on_batch=True)
+        scheduler = MultiStepLR(milestones=10, gamma=0.4, step_on_batch=True)
         scheduler.on_start(state)
 
-        lr_mock.assert_called_once_with('optimizer', 10, gamma=0.4, last_epoch=-4)
+        lr_mock.assert_called_once_with('optimizer', milestones=10, gamma=0.4, last_epoch=-1)
         self.assertTrue(scheduler._step_on_batch)
 
 
 class TestExponentialLR(TestCase):
     @patch('torch.optim.lr_scheduler.ExponentialLR')
     def test_lambda_lr(self, lr_mock):
-        state = {torchbearer.OPTIMIZER: 'optimizer'}
+        state = {torchbearer.OPTIMIZER: 'optimizer', torchbearer.EPOCH: 0}
 
-        scheduler = ExponentialLR(0.4, last_epoch=-4, step_on_batch=True)
+        scheduler = ExponentialLR(gamma=0.4, step_on_batch=True)
         scheduler.on_start(state)
 
-        lr_mock.assert_called_once_with('optimizer', 0.4, last_epoch=-4)
+        lr_mock.assert_called_once_with('optimizer', gamma=0.4, last_epoch=-1)
         self.assertTrue(scheduler._step_on_batch)
 
 
 class TestCosineAnnealingLR(TestCase):
     @patch('torch.optim.lr_scheduler.CosineAnnealingLR')
     def test_lambda_lr(self, lr_mock):
-        state = {torchbearer.OPTIMIZER: 'optimizer'}
+        state = {torchbearer.OPTIMIZER: 'optimizer', torchbearer.EPOCH: 0}
 
-        scheduler = CosineAnnealingLR(4, eta_min=10, last_epoch=-4, step_on_batch=True)
+        scheduler = CosineAnnealingLR(T_max=4, eta_min=10, step_on_batch=True)
         scheduler.on_start(state)
 
-        lr_mock.assert_called_once_with('optimizer', 4, eta_min=10, last_epoch=-4)
+        lr_mock.assert_called_once_with('optimizer', T_max=4, eta_min=10, last_epoch=-1)
         self.assertTrue(scheduler._step_on_batch)
 
 
 class TestReduceLROnPlateau(TestCase):
     @patch('torch.optim.lr_scheduler.ReduceLROnPlateau')
     def test_lambda_lr(self, lr_mock):
-        state = {torchbearer.OPTIMIZER: 'optimizer'}
+        state = {torchbearer.OPTIMIZER: 'optimizer', torchbearer.EPOCH: 0}
 
         scheduler = ReduceLROnPlateau(monitor='test', mode='max', factor=0.2, patience=100, verbose=True, threshold=10,
                                       threshold_mode='thresh', cooldown=5, min_lr=0.1, eps=1e-4, step_on_batch=True)
         scheduler.on_start(state)
 
-        lr_mock.assert_called_once_with('optimizer', mode='max', factor=0.2, patience=100, verbose=True, threshold=10,
-                                        threshold_mode='thresh', cooldown=5, min_lr=0.1, eps=1e-4)
+        lr_mock.assert_called_with('optimizer', mode='max', factor=0.2, patience=100, verbose=True, threshold=10,
+                                   threshold_mode='thresh', cooldown=5, min_lr=0.1, eps=1e-4, last_epoch=-1)
         self.assertTrue(scheduler._step_on_batch)
         self.assertTrue(scheduler._monitor == 'test')
 
@@ -386,21 +386,24 @@ class TestCyclicLR(TestCase):
         from distutils.version import LooseVersion
         import torch
         version = torch.__version__ if str(torch.__version__) is torch.__version__ else "0.4.0"
-        if LooseVersion(version) > LooseVersion("1.0.0"): # CyclicLR is implemented
+        if LooseVersion(version) > LooseVersion("1.0.0"):  # CyclicLR is implemented
             with patch('torch.optim.lr_scheduler.CyclicLR') as lr_mock:
-                state = {torchbearer.OPTIMIZER: 'optimizer'}
+                state = {torchbearer.OPTIMIZER: 'optimizer', torchbearer.EPOCH: 0}
 
-                scheduler = CyclicLR(0.01, 0.1, monitor='test', step_size_up=200, step_size_down=None, mode='triangular',
-                         gamma=2., scale_fn=None, scale_mode='cycle', cycle_momentum=False, base_momentum=0.7, max_momentum=0.9,
-                         last_epoch=-1, step_on_batch=True)
+                scheduler = CyclicLR(base_lr=0.01, max_lr=0.1, monitor='test', step_size_up=200, step_size_down=None,
+                                     mode='triangular', gamma=2., scale_fn=None, scale_mode='cycle',
+                                     cycle_momentum=False, base_momentum=0.7, max_momentum=0.9, step_on_batch=True)
                 scheduler.on_start(state)
 
-                lr_mock.assert_called_once_with('optimizer', 0.01, 0.1, step_size_up=200, step_size_down=None, mode='triangular',
-                         gamma=2., scale_fn=None, scale_mode='cycle', cycle_momentum=False, base_momentum=0.7, max_momentum=0.9,
-                         last_epoch=-1)
+                lr_mock.assert_called_once_with('optimizer', base_lr=0.01, max_lr=0.1, step_size_up=200,
+                                                step_size_down=None, mode='triangular', gamma=2., scale_fn=None,
+                                                scale_mode='cycle', cycle_momentum=False, base_momentum=0.7,
+                                                max_momentum=0.9, last_epoch=-1)
                 self.assertTrue(scheduler._step_on_batch)
                 self.assertTrue(scheduler._monitor == 'test')
         else:
-            self.assertRaises(NotImplementedError, lambda: CyclicLR(0.01, 0.1, monitor='test', step_size_up=200, step_size_down=None, mode='triangular',
-                         gamma=2., scale_fn=None, scale_mode='cycle', cycle_momentum=False, base_momentum=0.7, max_momentum=0.9,
-                         last_epoch=-1, step_on_batch=True))
+            self.assertRaises(NotImplementedError,
+                              lambda: CyclicLR(base_lr=0.01, max_lr=0.1, monitor='test', step_size_up=200,
+                                               step_size_down=None, mode='triangular', gamma=2., scale_fn=None,
+                                               scale_mode='cycle', cycle_momentum=False, base_momentum=0.7,
+                                               max_momentum=0.9, step_on_batch=True))
